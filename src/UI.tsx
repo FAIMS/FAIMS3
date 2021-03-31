1984b038b4644706bed21db7f89d53214ccf762e
James Tocknell <aragilar@gmail.com>
2021-03-31 16:42:54 +1100

    Vague outline of validation flow

--- src/UI.tsx
+++ src/UI.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Input from '@material-ui/core/Input';
 
 import {getComponentByName} from './ComponentRegistry';
 
@@ -78,51 +79,116 @@ type FormProps = {
 };
 
 type FormState = {
+    uiSpec: any;
+    currentView: string;
+    validateCallback: any;
 };
 
 export class FAIMSForm extends React.Component<FormProps,FormState> {
+    constructor(props) {
+        super(props)
+        let uiSpec = getUiSpecForProject(props.project);
+        this.state = {
+            uiSpec: uiSpec,
+            currentView: uiSpec["start-view"],
+            validateCallback: () => {return {};},
+        };
+        this.validate = this.validate.bind(this);
+    }
+
+    validate (event) {
+        this.save(this.state.validateCallback());
+        event.preventDefault(); // Stops the rerender of the page
+    }
+
+    save(values) {
+        console.log(values);
+    }
+
+    updateView(viewName) {
+        if (viewName in this.state.uiSpec["views"]) {
+            this.setState({currentView: viewName});
+            this.forceUpdate();
+            // Probably not needed, but we *know* we need to rerender when this
+            // changes, so let's be explicit.
+        } else {
+            throw Error(`No view ${viewName}`);
+        }
+    }
+
+    setValidateCallback(callback) {
+        this.setState({validateCallback: callback});
+    }
+
+    getComponentFromField(fieldName:string, view:ViewComponent) {
+        let uiSpec = this.state.uiSpec;
+        let fields = uiSpec["fields"];
+        return this.getComponentFromFieldConfig(fields[fieldName], view);
+    }
+
+    getComponentFromFieldConfig(fieldConfig: any, view:ViewComponent) {
+        let Component = getComponentByName(
+            fieldConfig["component-namespace"], fieldConfig["component-name"]
+        );
+        return (
+            <Component view={view} {...fieldConfig["component-parameters"]} />
+        );
+    }
 
     render() {
-        let project = this.props.project;
-        let uiSpec = getUiSpecForProject(project);
-        let viewName = uiSpec["start-view"];
-        let fields = uiSpec["fields"];
-        let viewList = uiSpec["views"][viewName]["fields"];
+        let uiSpec = this.state.uiSpec;
+        let viewName = this.state.currentView;
+        let viewList: Array<string> = uiSpec["views"][viewName]["fields"];
         return (
-            <div>
+            <form onSubmit={this.validate}>
             <p>{this.props.project} is the current project</p>
-            <ViewComponent viewList={viewList} fields={fields} />
-            </div>
+            <ViewComponent viewList={viewList} form={this} />
+            <Input type="submit" value="Save" />
+            </form>
         );
     }
 }
 
 type ViewProps = {
     viewList: any;
-    fields: any;
+    form: FAIMSForm;
 };
 
 type ViewState = {
 };
 
 export class ViewComponent extends React.Component<ViewProps,ViewState> {
+    constructor(props) {
+        super(props)
+        this.validate = this.validate.bind(this);
+        let form = this.props.form;
+        form.setValidateCallback(this.validateCallback);
+    }
 
+    validateCallback () {
+        console.log("Doing the validation");
+        return {};
+    }
 
-    getComponentFromField(fieldConfig: any) {
-        let Component = getComponentByName(
-            fieldConfig["component-namespace"], fieldConfig["component-name"]
-        );
-        return (
-            <Component {...fieldConfig["component-parameters"]} />
-        );
+    validate(event) {
+        this.save(this.validateCallback());
+        event.preventDefault(); // Stops the rerender of the page
+    }
+
+    save(values) {
+        console.log(values);
+    }
+
+    getForm() {
+        return this.props.form;
     }
 
     render() {
-        let fields = this.props.fields;
+        let form = this.props.form;
         return (
             <>
                 {this.props.viewList.map((fieldName) => {
-                    return this.getComponentFromField(fields[fieldName]);
+                    return form.getComponentFromField(fieldName, this);
                 })}
             </>
         );
