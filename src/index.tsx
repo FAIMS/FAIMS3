dfc7ea33186e8fce6d297867adba688934cb06ef
Aidan Farrell <aidan.farrell@mq.edu.au>
2021-04-07 10:21:31 +1000

    Add sync() initialization to the main react index

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sync from './sync/index';
+
+// TODO: show react (loading bar?) and Sync the DBs
+// at the same time (Making sure DB calls can handle this)
+async function initialize() {
+
+  await Sync.populate_test_data();
+
+  await Sync.initialize_dbs({
+      host: '10.80.11.44',
+      port: 5984,
+      db_name: 'directory'
+  });
+
+}
+initialize().then(() => {
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +26,16 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+}).catch(e => {
+  // TODO: Prettier error
+
+  ReactDOM.render(
+    <p>Error: {e.toString()}</p>,
+    document.getElementById('root')
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
