93bc61a8cded0f01899f7725a98d2533df360416
riniangreani <rini.angreani@csiro.au>
2021-05-12 14:35:49 +0800

    Updated TestUIForm with latest main - test passed.

--- e2e/src/test/java/org/fedarch/faims3/android/TestUIForm.java
+++ e2e/src/test/java/org/fedarch/faims3/android/TestUIForm.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -43,37 +44,58 @@ public class TestUIForm {
         ExpectedConditions.elementToBeClickable(MobileBy.xpath("//*[@resource-id='email-field']")));
     emailField.sendKeys("jane.doe@csiro.au");
 
-    // String field
+    // Colour field
     AndroidElement strField = driver.findElement(MobileBy.xpath("//*[@resource-id='str-field']"));
-    strField.sendKeys("Some text");
+    strField.sendKeys("Pink");
 
-    // Scroll down until you can see number field, and then fill it in
-    AndroidElement intField = TestUtils.scrollDownUntilVisible(By.xpath("//*[@resource-id='int-field']"), driver);
+    // Scroll down until you can see text area field, and then fill it
+    AndroidElement textField = TestUtils.scrollDownUntilVisible(MobileBy.xpath("//*[@resource-id='multi-str-field']"), driver);
+    textField.sendKeys("いろはにほへとちりぬるを Pchnąć w tę łódź jeża lub ośm skrzyń fig จงฝ่าฟันพัฒนาวิชาการ    côté de l'alcôve ovoïde größeren");
+
+    // Number field
+    AndroidElement intField = driver.findElement(By.xpath("//*[@resource-id='int-field']"));
     intField.sendKeys("16");
 
     // Currency field
-    AndroidElement currencyField = driver.findElement(MobileBy.xpath("//*[@resource-id='select-field' and @text='Choose a currency']"));
+    AndroidElement currencyField = driver.findElement(MobileBy.xpath("//*[@resource-id='select-field' and @text='Currency']"));
     currencyField.click();
-    //TODO: wait for it to load
-    List<AndroidElement> values = driver.findElements(MobileBy.xpath("//android.widget.ListView[@text='Choose a currency']/android.view.View"));
+    // wait for list of currencies to load
+    List<WebElement> values = new WebDriverWait(driver, 10).until(
+            ExpectedConditions.visibilityOfAllElementsLocatedBy(
+            		MobileBy.xpath("//android.widget.ListView[@text='Currency']/android.view.View")));
     // choose the first value: '$'
     values.get(0).click();
 
     // Multiple currency field
     AndroidElement multiCurrField = (AndroidElement) new WebDriverWait(driver, 10).until(
-            ExpectedConditions.elementToBeClickable(MobileBy.xpath("//*[@resource-id='multi-select-field' and @text='Choose multiple currencies']")));
+            ExpectedConditions.elementToBeClickable(
+            		MobileBy.xpath("//*[@resource-id='multi-select-field' and @text='Currencies']")));
     multiCurrField.click();
-    values = driver.findElements(MobileBy.xpath("//android.widget.ListView[@text='Choose multiple currencies']/android.view.View"));
+    values = new WebDriverWait(driver, 10).until(
+            ExpectedConditions.visibilityOfAllElementsLocatedBy(
+            		MobileBy.xpath("//android.widget.ListView[@text='Currencies']/android.view.View")));
     // choose first, second, and fourth: $, euro and Yen
     values.get(0).click();
     values.get(1).click();
     values.get(3).click();
 
+    // click on integer field above to hide the currency selection (would be covering the checkbox below)
+    intField.click();
+
+    // tick the checkbox
+    AndroidElement checkbox = TestUtils.scrollDownUntilVisible(MobileBy.xpath("//*[@resource-id='checkbox-field']"), driver);
+    checkbox.click();
+
+    // radio button
+    AndroidElement radioField = driver.findElement(MobileBy.xpath("//*[@resource-id='radio-group-field']"));
+    // click the fourth one
+    radioField.findElementsByClassName("android.view.View").get(3).findElementByClassName("android.widget.RadioButton").click();
+
     // Submit button
-    AndroidElement submit = driver.findElement(By.xpath("//*[@text='SUBMIT'"));
+    AndroidElement submit = driver.findElement(By.xpath("//*[@text='SUBMIT']"));
     submit.click();
 
-    // Validate JSON
+    //TODO: Validate JSON
 
     // The driver.quit statement is required, otherwise the test continues to execute, leading to a timeout.
     driver.quit();
