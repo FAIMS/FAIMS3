d77725d682c581ca39bdcdd01162b6f25d28c093
riniangreani <rini.angreani@csiro.au>
2021-05-26 10:26:25 +0800

    FAIMS3-219: update e2e tests with GPS point testing
    Lake Mungo has a "Take Point" button that generates lat long.

--- e2e/src/test/java/org/fedarch/faims3/android/TestStagingForm.java
+++ e2e/src/test/java/org/fedarch/faims3/android/TestStagingForm.java
@@ -29,7 +29,7 @@ public class TestStagingForm extends AndroidTest {
   public static void setup() throws MalformedURLException {
 	  // Test with browserstack by default
 	  // Change to true for local test connection
-	  AndroidTest.setup(false, "Test Form Staging - Android");
+	  AndroidTest.setup(true, "Test Form Staging - Android");
   }
 
   /**
@@ -55,24 +55,27 @@ public class TestStagingForm extends AndroidTest {
 			// validate JSON values
 			LakeMungo.validateJSON(driver);
 			// scroll up and click on the "Example Project A" tab
-			TestUtils.scrollUpUntilVisible(MobileBy.xpath("//*[@resource-id='project-nav-scrollable-tab-projectB']"), driver).click();
+			TestUtils.scrollToResourceId(driver, "project-nav-scrollable-tab-projectB").click();
 			// click submit before opening Lake Mungo tab again
 			WebDriverWait wait = new WebDriverWait(driver, 10);
 			wait.until(ExpectedConditions.elementToBeClickable(MobileBy.xpath("//*[@text='SUBMIT']"))).click();;
 			// reopen Lake Mungo
 			driver.findElement(MobileBy.xpath("//*[@resource-id='project-nav-scrollable-tab-lake_mungo']")).click();
 			// Check all fields are still the same
+			LakeMungo.validateLatLong(driver);
 			assertEquals(LakeMungo.EMAIL, driver.findElement(MobileBy.xpath("//*[@resource-id='email-field']")).getText());
 			assertEquals(LakeMungo.COLOUR, driver.findElement(MobileBy.xpath("//*[@resource-id='str-field']")).getText());
 
-			AndroidElement currencies = TestUtils.scrollDownUntilVisible(MobileBy.xpath("//*[@resource-id='multi-str-field']"), driver);
+			AndroidElement currencies = TestUtils.scrollToResourceId(driver, "multi-str-field");
 			assertEquals(LakeMungo.UNICODE, currencies.getText());
 
 			TestUtils.scrollDown(driver);
 
-			assertEquals("16.0", driver.findElement(MobileBy.xpath("//*[@resource-id='int-field']")).getText());
+			assertEquals("1.0", wait.until(
+					ExpectedConditions.visibilityOfElementLocated(
+							MobileBy.xpath("//*[@resource-id='int-field']"))).getText());
 			assertEquals("Currency €", driver.findElement(MobileBy.xpath("//*[@resource-id='select-field']")).getText());
-			assertEquals("Currencies $, €, ¥", driver.findElement(MobileBy.xpath("//*[@resource-id='multi-select-field']")).getText());
+			assertEquals("Currencies $, €", driver.findElement(MobileBy.xpath("//*[@resource-id='multi-select-field']")).getText());
 			assertEquals("true", driver.findElement(MobileBy.xpath("//*[@resource-id='checkbox-field']")).getAttribute("checked"));
 			for (AndroidElement radioButton : driver.findElementsByClassName("android.widget.RadioButton")) {
 				if (radioButton.getText().equals("4")) {
@@ -87,18 +90,21 @@ public class TestStagingForm extends AndroidTest {
 
 			// Make sure JSON is still the same
 			if (!LakeMungo.validateJSON(driver)) {
-				TestUtils.markBrowserstackTestResult(driver, false, "Android - TestStagingForm.testSwitchTab() fails because JSON values don't match.");
+				TestUtils.markBrowserstackTestResult(driver, isUsingBrowserstack(), false,
+						"Android - TestStagingForm.testSwitchTab() fails because JSON values don't match.");
 				return;
 			}
 	  } catch (Exception e) {
-	      TestUtils.markBrowserstackTestResult(driver, false, "Exception " + e.getClass().getSimpleName() + " occurs! See log for details.");
+	      TestUtils.markBrowserstackTestResult(driver, isUsingBrowserstack(), false,
+	    		  "Exception " + e.getClass().getSimpleName() + " occurs! See log for details.");
 	      throw e;
 	  } catch (AssertionError e) {
-		  TestUtils.markBrowserstackTestResult(driver, false, "Assertion Error: '" + e.getMessage() + "' occurs! See log for details.");
+		  TestUtils.markBrowserstackTestResult(driver, isUsingBrowserstack(), false,
+				  "Assertion Error: '" + e.getMessage() + "' occurs! See log for details.");
 	      throw e;
 	  }
 	  // if we make it to the end with no exceptions, that means we passed!
-	  TestUtils.markBrowserstackTestResult(driver, true, "Android - TestStagingForm.testSwitchTab() passed!");
+	  TestUtils.markBrowserstackTestResult(driver, isUsingBrowserstack(), true, "Android - TestStagingForm.testSwitchTab() passed!");
   }
 
   //TODO: switch via menu on the left
