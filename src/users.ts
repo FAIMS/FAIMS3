6fc8bea91e71110f3564e9bdd4a714563c5a6692
James Tocknell <aragilar@gmail.com>
2021-06-01 12:43:49 +1000

    Linting fixes
    Signed-off-by: James Tocknell <aragilar@gmail.com>
    Signed-off-by: riniangreani <rini.angreani@csiro.au>

--- src/users.ts
+++ src/users.ts
@@ -26,12 +26,12 @@ export async function getFriendlyUserName(
 ): Promise<string> {
   const doc = await active_db.get(project_id);
   if (doc.friendly_name === undefined) {
-    return doc.username || "Dummy User";
+    return doc.username || 'Dummy User';
   }
   return doc.friendly_name;
 }
 
 export async function getCurrentUserId(project_id: ProjectID): Promise<string> {
   const doc = await active_db.get(project_id);
-  return doc.username || "Dummy User";
+  return doc.username || 'Dummy User';
 }
