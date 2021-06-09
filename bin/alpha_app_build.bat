89d1af037ebb5a72b31d4fbbcaba078ec2c17b36
riniangreani <rini.angreani@csiro.au>
2021-06-09 19:30:15 +0800

    alpha build script works for windows
    Signed-off-by: riniangreani <rini.angreani@csiro.au>

--- bin/alpha_app_build.bat
+++ bin/alpha_app_build.bat
@@ -1,9 +1,11 @@
-setx REACT_APP_PRODUCTION_BUILD=true
-setx REACT_APP_DIRECTORY_HOST=alpha.db.faims.edu.au
-setx REACT_APP_COMMIT_VERSION=rini-windows
+setx REACT_APP_PRODUCTION_BUILD true /M
+setx REACT_APP_DIRECTORY_HOST alpha.db.faims.edu.au /M
+for /f "delims=" %i in ('git describe --long --all') do setx REACT_APP_COMMIT_VERSION %i /M
 
 git clean -xfd 
 npm ci 
-npx cap copy android
-npx cap update android
+npm run build
+npx cap sync android
 npx cap open android
+
+
