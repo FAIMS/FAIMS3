53269a40d8901496c03c16e0c29ce49c9a8a3673
Aidan Farrell <aidan.farrell@mq.edu.au>
2021-06-15 13:02:02 +1000

    Disable the asyncToGenerator.js babel plugin for a _far_ easier debugging experience
    Signed-off-by: Aidan Farrell <aidan.farrell@mq.edu.au>

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,7 @@
+const { override, removeInternalBabelPlugin } = require('customize-cra');
 
 module.exports = function override(config, env) {
+    config = removeInternalBabelPlugin('@babel/plugin-transform-async-to-generator')(config, env);
     config.optimization.minimize = false;
     return config;
 }
