8149c221ef905db3c67cb79b4dfc75788a39a88b
Aidan Farrell <aidan.farrell@mq.edu.au>
2021-08-09 10:56:30 +1000

    Signin form elements
    Signed-off-by: Aidan Farrell <aidan.farrell@mq.edu.au>

--- src/gui/components/authentication/cluster_card.tsx
+++ src/gui/components/authentication/cluster_card.tsx
@@ -19,30 +19,126 @@
  */
 
 import {
+  Box,
+  Button,
   Card as MuiCard,
   CardActions,
   CardContent,
   CardHeader,
+  CircularProgress,
+  FormControl,
+  Grid,
+  Input,
+  InputLabel,
+  MenuItem,
+  Select,
 } from '@material-ui/core';
 
 import {makeStyles} from '@material-ui/core/styles';
+import {useEffect} from 'react';
+import {useState} from 'react';
+import {LocalAuthDoc} from '../../../datamodel';
+import {local_auth_db} from '../../../sync/databases';
 
 type ClusterCardProps = {
   listing_id: string;
 };
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(theme => ({
   cardHeader: {
     alignItems: 'flex-start',
   },
+  margin: {
+    'margin-top': theme.spacing(2),
+  },
 }));
+
+function mapFullState<T, S>(
+  fullState: null | T | {error: {}},
+  ok: (val: T) => S,
+  err: (err: {}) => S,
+  loading: () => S
+) {
+  if (fullState === null) {
+    return loading();
+  } else if ('error' in fullState) {
+    return err(fullState.error);
+  } else {
+    return ok(fullState);
+  }
+}
 
 export default function ClusterCard(props: ClusterCardProps) {
   const classes = useStyles();
+  const [authDBDoc, setAuthDBDoc] = useState(
+    null as null | LocalAuthDoc | {error: {}}
+  );
+  useEffect(() => {
+    local_auth_db.get(props.listing_id).then(setAuthDBDoc, (err: any) => {
+      setAuthDBDoc({error: err});
+    });
+
+    const changes = local_auth_db.changes({include_docs: true, since: 'now'});
+    const change_listener = (
+      change: PouchDB.Core.ChangesResponseChange<LocalAuthDoc>
+    ) => {
+      setAuthDBDoc(change.doc!);
+    };
+    const error_listener = (err: any) => {
+      setAuthDBDoc({error: err});
+    };
+    changes.on('change', change_listener);
+    changes.on('error', error_listener);
+    return () => {
+      changes.removeListener('change', change_listener);
+      changes.removeListener('error', error_listener);
+    };
+  });
+
   return (
     <MuiCard>
       <CardHeader className={classes.cardHeader} title={props.listing_id} />
-      <CardContent style={{paddingTop: 0}}></CardContent>
+      <CardContent style={{paddingTop: 0}}>
+        {mapFullState(
+          authDBDoc,
+          authDBDoc => (
+            <Box>
+              <FormControl className={classes.margin} fullWidth>
+                <Select
+                  labelId="auth-method"
+                  id="auth-method"
+                  value={'dc_password'}
+                  fullWidth
+                >
+                  <MenuItem value={'dc_password'}>Data Central</MenuItem>
+                </Select>
+              </FormControl>
+              <FormControl className={classes.margin} fullWidth>
+                <InputLabel htmlFor="dc-username">Username</InputLabel>
+                <Input id="dc-username" fullWidth />
+              </FormControl>
+              <FormControl className={classes.margin} fullWidth>
+                <InputLabel htmlFor="dc-password">Password</InputLabel>
+                <Input type="password" id="dc-password" fullWidth />
+              </FormControl>
+              <Button
+                className={classes.margin}
+                variant="contained"
+                color="primary"
+                fullWidth
+              >
+                Login
+              </Button>
+            </Box>
+          ),
+          err => (
+            <pre>Error: {err.toString()}</pre>
+          ),
+          () => (
+            <CircularProgress color="primary" size="2rem" thickness={5} />
+          )
+        )}
+      </CardContent>
       <CardActions></CardActions>
     </MuiCard>
   );
