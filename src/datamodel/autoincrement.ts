2118a35667b3ab93f8850c304865e1a35f455584
James Tocknell <aragilar@gmail.com>
2021-08-30 17:41:03 +1000

    Fix licensing
    Signed-off-by: James Tocknell <aragilar@gmail.com>

--- src/datamodel/autoincrement.ts
+++ src/datamodel/autoincrement.ts
@@ -1,3 +1,28 @@
+/*
+ * Copyright 2021 Macquarie University
+ *
+ * Licensed under the Apache License Version 2.0 (the, "License");
+ * you may not use, this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND either express or implied.
+ * See, the License, for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Filename: autoincrement.ts
+ * Description:
+ *   TODO
+ */
+
+// There are two internal IDs for projects, the former is unique to the system
+// (i.e. includes the listing_id), the latter is unique only to the 'projects'
+// database it came from, for a FAIMS listing
+// (It is this way because the list of projects is decentralised and so we
+// cannot enforce system-wide unique project IDs without a 'namespace' listing id)
 import {getProjectDB} from '../sync/index';
 import {local_state_db} from '../sync/databases';
 import {ProjectID} from './core';
