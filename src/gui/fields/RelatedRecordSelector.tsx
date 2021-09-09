95758f3b1d36944ee25949ec86a34f59d233da82
James Tocknell <aragilar@gmail.com>
2021-09-09 17:01:05 +1000

    Add search functionality to RelatedRecordSelector
    This is currently not doing anything with the relationship type. It
    allows multiple records to be selected, which may or may not make sense
    depending on the context.
    
    I did also switch this from a class-based component to a function-based
    one, given the nature of the MUI Autocomplete component.
    
    Signed-off-by: James Tocknell <aragilar@gmail.com>

--- src/gui/fields/RelatedRecordSelector.tsx
+++ src/gui/fields/RelatedRecordSelector.tsx
@@ -22,41 +22,72 @@ import React from 'react';
 import {Link as RouterLink} from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
-
-import {fieldToTextField, TextFieldProps} from 'formik-material-ui';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import {FieldProps} from 'formik';
 
 import * as ROUTES from '../../constants/routes';
+import {FAIMSTypeName} from '../../datamodel/core';
+import {RecordReference} from '../../datamodel/ui';
+import {getAllRecordsOfType} from '../../data_storage/queries';
 
 interface Props {
-  related_type: string;
-  relation_type: string;
+  related_type: FAIMSTypeName;
+  relation_type: FAIMSTypeName;
 }
 
-export class RelatedRecordSelector extends React.Component<
-  TextFieldProps & Props
-> {
-  render() {
-    const {related_type, relation_type, ...textFieldProps} = this.props;
-    const project_id = this.props.form.values['_project_id'];
-    return (
+export function RelatedRecordSelector(props: FieldProps & Props) {
+  const project_id = props.form.values['_project_id'];
+  const [options, setOptions] = React.useState<RecordReference[]>([]);
+
+  React.useEffect(() => {
+    (async () => {
+      const records = await getAllRecordsOfType(project_id, props.related_type);
+      setOptions(records);
+    })();
+  }, []);
+
+  // Note the "multiple" option below, that seems to control whether multiple
+  // entries can in entered.
+  // TODO: Have the relation_type set the multiplicity of the system
+  return (
+    <div>
+      <Autocomplete
+        multiple
+        id="asynchronous-demo"
+        style={{width: 300}}
+        getOptionSelected={(option, value) =>
+          option.project_id === value.project_id &&
+          option.record_id === value.record_id
+        }
+        getOptionLabel={option => option.record_label}
+        options={options}
+        renderInput={params => (
+          <TextField
+            {...params}
+            label="Asynchronous"
+            variant="outlined"
+            InputProps={{
+              ...params.InputProps,
+            }}
+          />
+        )}
+      />
       <Button
         variant="outlined"
         color="primary"
         startIcon={<AddIcon />}
-        // If the list of views hasn't loaded yet
-        // we can still show this button, except it will
-        // redirect to the Record creation without known type
         component={RouterLink}
         to={
           ROUTES.PROJECT +
           project_id +
           ROUTES.RECORD_CREATE +
           ROUTES.RECORD_TYPE +
-          related_type
+          props.related_type
         }
       >
         New Record
       </Button>
-    );
-  }
+    </div>
+  );
 }
