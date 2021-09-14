7b148dfeddafd46b1b9d245ebf3a1a27dbb43032
Aidan Farrell <aidan.farrell@mq.edu.au>
2021-09-14 15:04:45 +1000

    Fix one of the ingration tests failing because Error isn't a constructor..(?)
    Signed-off-by: Aidan Farrell <aidan.farrell@mq.edu.au>

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -20,7 +20,7 @@
 import {ReportHandler} from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
     import('web-vitals').then(({getCLS, getFID, getFCP, getLCP, getTTFB}) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
