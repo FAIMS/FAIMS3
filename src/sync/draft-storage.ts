291dcfd385ea6d800d85f555facf82be2911b25e
Aidan Farrell <aidan.farrell@mq.edu.au>
2021-09-28 15:19:59 +1000

    Completley re-jig and simplify draft saving to allow multiple drafts, discriminated by their own draft ID now
    Signed-off-by: Aidan Farrell <aidan.farrell@mq.edu.au>
    
    Signed-off-by: James Tocknell <aragilar@gmail.com>

--- src/sync/draft-storage.ts
+++ src/sync/draft-storage.ts
@@ -21,101 +21,123 @@
 import PouchDB from 'pouchdb';
 import {RecordID, ProjectID, RevisionID} from '../datamodel/core';
 import {SavedView} from '../datamodel/drafts';
+import {v4 as uuidv4} from 'uuid';
 
 export type DraftDB = PouchDB.Database<SavedView>;
 
 export const draft_db: DraftDB = new PouchDB('draft-storage');
 
 export async function getStagedData(
-  active_id: ProjectID,
-  // Revision & record from data db.
-  existing_record_id: RecordID | null,
-  existing_revision_id: RevisionID | null
-): Promise<null | (SavedView & PouchDB.Core.GetMeta)> {
-  const _id = determineId(active_id, existing_record_id, existing_revision_id);
-
-  try {
-    return await draft_db.get(_id);
-  } catch (err: any) {
-    if (err.message !== 'missing') {
-      throw err;
-    }
-    // MISSING OBJECT
-    return null;
-  }
+  draft_id: string
+): Promise<SavedView & PouchDB.Core.GetMeta> {
+  return await draft_db.get(draft_id);
 }
 
 /**
+ *
+ * @param active_id ID of the project that this draft is for
+ * @param existing If this draft is a draft of edits to an existing record,
+ *                 these IDs identify the existing record
+ * @param type (Less useful if existing is set, as this can be fetched from
+ *              other pouch databases if existing data is set) The FAIMS type
+ *             of this draft document. Doesn't change when draft is updated.
+ * @returns ID of the newly created draft. Use through URL routes to eventually
+ *          call [get|set]stagedData with
+ */
+export async function newStagedData(
+  active_id: ProjectID,
+  existing: null | {
+    record_id: RecordID;
+    revision_id: RevisionID;
+  },
+  type: string
+): Promise<PouchDB.Core.DocumentId> {
+  const _id = uuidv4();
+  const date = new Date();
+
+  return (
+    await draft_db.put({
+      _id: _id,
+      created: date.toString(),
+      updated: date.toString(),
+      fields: {},
+      project_id: active_id,
+      existing: existing,
+      type: type,
+    })
+  ).id;
+}
+
+/**
+ * @param draft_id This is from the routed URL parameter, which is redirected
+ *                 to from the list of drafts or the newStagedData. The ID
+ *                 that identifies the draft you want to update.
  * @param new_data Data to update/insert
- * @param {string|null} _rev _rev of last SavedView object to use instead of all the 4 id's
- *                       Using this is more efficient, since a lookup for _rev doesn't need to happen.
- *                       Can be null if either this is the first staged value of this field,
- *                       or if _rev is not yet known
- * @param {string} active_id First arg: active_id of a project from ActiveDB
- * @param {string|null} existing_record Record id if this was editing an existing record
- * @param {string|null} existing_revision Revision ID if this was editing an existing revision
  */
 export async function setStagedData(
-  new_data: {[key_not_underscore_id: string]: unknown},
-  _rev: string | null,
-  active_id: ProjectID,
-  existing_record_id: RecordID | null,
-  existing_revision_id: RevisionID | null
+  // Matches the PouchDB.Core.Response type, but with optional rev
+  draft_id: PouchDB.Core.DocumentId,
+  new_data: {[key: string]: unknown}
 ): Promise<PouchDB.Core.Response> {
-  const _id = determineId(active_id, existing_record_id, existing_revision_id);
-  try {
-    const put_doc: PouchDB.Core.PutDocument<SavedView> = {
-      fields: new_data,
-      _id: _id,
-    };
-    if (_rev !== null) put_doc._rev = _rev;
+  const existing = await draft_db.get(draft_id);
 
-    return await draft_db.put(put_doc);
-  } catch (possibleConflict: any) {
-    if (possibleConflict.name === 'conflict') {
-      // Do it less efficiently if a conflict occurs, by doing a _rev lookup first.
-      console.warn(`Conflict in local draft db: ${possibleConflict}`);
-      const _rev = (await draft_db.get(_id))._rev;
-      return await setStagedData(
-        new_data,
-        _rev,
-        active_id,
-        existing_record_id,
-        existing_revision_id
-      );
-    } else {
-      throw possibleConflict;
-    }
-  }
+  return await draft_db.put({
+    ...existing,
+    fields: new_data,
+    updated: new Date().toString(),
+  });
 }
 
 /**
- * Generates a deterministic "_id" for SavedView structs
- * based on the given parameters.
  *
- * Used for when you're trying to save or retrieve something
- * to/from the draft area, this gives you a pouch unique _id to use.
- *
- * If you're trying to get/save data for when the user
- * is creating a new document, leave out existing_record or existing_revision
- * (the last 2 parameters)
- *
- * @param {string} active_id First arg: active_id of a project from ActiveDB
- * @param {string|null} existing_record Record id if this was editing an existing record
- * @param {string|null} existing_revision Revision ID if this was editing an existing revision
- * @returns {string} _id field for pouch SavedView
+ * @param draft_id Identifies the draft to delete
+ * @param revision_cache To avoid having to look it up, if you know the revision
+ *                       id of the draft, pass it through here.
  */
-function determineId(
-  active_id: ProjectID,
-  existing_record_id: RecordID | null,
-  existing_revision_id: RevisionID | null
-): string {
-  const parts: string[] = [active_id];
-  if (existing_record_id !== null) {
-    parts.push(existing_record_id);
-  }
-  if (existing_revision_id !== null) {
-    parts.push(existing_revision_id);
-  }
-  return parts.map(encodeURIComponent).join('/');
+export async function deleteStagedData(
+  draft_id: PouchDB.Core.DocumentId,
+  revision_cache: null | PouchDB.Core.RevisionId
+) {
+  const revision =
+    revision_cache !== null
+      ? revision_cache
+      : (await draft_db.get(draft_id))._rev;
+
+  await (draft_db as PouchDB.Database<{}>).put({
+    _id: draft_id,
+    _rev: revision,
+    _deleted: true,
+  });
+}
+
+/**
+ *
+ * @param project_id Project ID (fully qualified) to get the list of drafts for
+ * @param filter Determines what subset (or all) of the drafts to get.
+ *               This filters by what type of draft.
+ *               If a given draft is an update to an existing record that has
+ *               been synced, it will show when listing by 'all' or 'updates'.
+ *               Otherwise, it will show when listing by 'all' or 'created'.
+ * @returns List of drafts filtered.
+ */
+export async function listStagedData(
+  project_id: string,
+  filter: 'updates' | 'created' | 'all'
+): Promise<SavedView[]> {
+  return (
+    await draft_db.find({
+      selector: {
+        project_id: project_id,
+        // Based on what value filter takes, we either:
+        // all: Don't select on the existing field at all,
+        // created: Select for null existing fields, or
+        // updates: Select for non-null existing fields
+        ...(filter === 'all'
+          ? {}
+          : {
+              existing: filter === 'updates' ? {$ne: null} : null,
+            }),
+      },
+    })
+  ).docs;
 }
