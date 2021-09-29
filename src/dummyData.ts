7944c56db352d36c6b73d0c6311736273b4da36f
Aidan Farrell <aidan.farrell@mq.edu.au>
2021-09-29 14:11:27 +1000

    Dummy data now starts off with HRID field set, (So when you open it to view it, it won't immediately create a new draft)
    Signed-off-by: Aidan Farrell <aidan.farrell@mq.edu.au>

--- src/dummyData.ts
+++ src/dummyData.ts
@@ -50,6 +50,8 @@ const example_records: {
       updated: randomDate(new Date(2021, 0, 1), new Date()),
       updated_by: 'Yoda',
       data: {
+        'basic-autoincrementer-field': 1,
+        'hrid-field': 'αβγ blurple-1',
         'take-point-field': {latitude: -33.7964844, longitude: 151.1456739},
         'bad-field': '',
         'action-field': 'hello',
@@ -73,6 +75,8 @@ const example_records: {
       updated: randomDate(new Date(2021, 0, 1), new Date()),
       updated_by: 'Yoda',
       data: {
+        'basic-autoincrementer-field': 2,
+        'hrid-field': 'αβγ blurple-2',
         'take-point-field': {latitude: -33.7964844, longitude: 151.1456739},
         'bad-field': '',
         'action-field': 'hello',
