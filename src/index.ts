bd0aa1cb99cf09123d44cefe0a90cc96dfadc32d
Aidan Farrell <aidan.farrell@mq.edu.au>
2021-10-01 09:47:08 +1000

    Update index.ts with the Auth IDs
    Signed-off-by: Aidan Farrell <aidan.farrell@mq.edu.au>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import {users_db} from './sync/databases';
+import {self_listing_info, users_db} from './sync/databases';
 import passport from 'passport';
 import OAuth2Strategy from 'passport-oauth2';
 import {initialize} from './sync/initialize';
@@ -12,47 +12,17 @@ import {
   register_metas_complete,
   register_projects_created,
 } from './sync/state';
+import {auth_mechanisms} from './authconfig';
 
 process.on('unhandledRejection', error => {
   console.error(error); // This prints error with stack included (as for normal errors)
   throw error; // Following best practices re-throw error and let the process exit with error code
 });
 
-const auth_strategies: {[listing_id: string]: OAuth2Strategy} = {};
-
-passport.use(
-  'default',
-  new OAuth2Strategy(
-    {
-      authorizationURL:
-        'https://auth.datacentral.org.au/cas/oauth2.0/authorize',
-      tokenURL: 'https://auth.datacentral.org.au/cas/oauth2.0/accessToken',
-      clientID: '5c1dca8c5c10f7b96f50e5829816a260-datacentral.org.au',
-      clientSecret:
-        '3478721c4c92e9e6118aaa315712854087ebc4b01abb9e7977bd17dc66d0c67c',
-      callbackURL: 'http://localhost:3000/signin-return/',
-    },
-    (
-      accessToken: string,
-      refreshToken: string,
-      profile: any,
-      cb: (err?: Error | null, user?: Express.User, info?: unknown) => void
-    ) => {
-      console.debug(
-        accessToken,
-        refreshToken,
-        profile,
-        JSON.stringify(profile)
-      );
-      cb(null, undefined, undefined);
-    }
-  )
-);
-
 const app = express();
 
-app.get('/auth/:listing_id', (req, res) => {
-  passport.authenticate(req.params.listing_id)(req, res);
+app.get('/auth/:auth_id', (req, res) => {
+  passport.authenticate(req.params.auth_id)(req, res);
 });
 
 app.get(
@@ -75,7 +45,29 @@ add_initial_listener(register_projects_known, 'projects_known');
 add_initial_listener(register_metas_complete);
 add_initial_listener(register_projects_created);
 
-initialize().then(() => {
+initialize().then(async () => {
+  for (const auth_id in auth_mechanisms) {
+    passport.use(
+      'default',
+      new OAuth2Strategy(
+        auth_mechanisms[auth_id].strategy,
+        (
+          accessToken: string,
+          refreshToken: string,
+          profile: any,
+          cb: (err?: Error | null, user?: Express.User, info?: unknown) => void
+        ) => {
+          console.debug(
+            accessToken,
+            refreshToken,
+            profile,
+            JSON.stringify(profile)
+          );
+          cb(null, undefined, undefined);
+        }
+      )
+    );
+  }
   app.listen(8080, () => {
     console.log('The hello is listening on port 8080!');
   });
