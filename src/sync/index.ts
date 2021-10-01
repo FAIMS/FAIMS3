08439cf2d0700ab014749778ff9d7fd06fbd9092
Aidan Farrell <aidan.farrell@mq.edu.au>
2021-10-01 14:19:40 +1000

    Add listener sibling functions to getDataDB & getProjectDB
    Signed-off-by: Aidan Farrell <aidan.farrell@mq.edu.au>

--- src/sync/index.ts
+++ src/sync/index.ts
@@ -23,8 +23,8 @@ import PouchDB from 'pouchdb';
 import PouchDBFind from 'pouchdb-find';
 import {ProjectID} from '../datamodel/core';
 import {ProjectDataObject, ProjectMetaObject} from '../datamodel/database';
-import {data_dbs, metadata_dbs} from './databases';
-import {all_projects_updated} from './state';
+import {data_dbs, ExistingActiveDoc, metadata_dbs} from './databases';
+import {all_projects_updated, createdProjectsInterface} from './state';
 
 PouchDB.plugin(PouchDBFind);
 
@@ -50,6 +50,78 @@ export async function getDataDB(
   }
 }
 
+/**
+ * Allows you to listen for changes from a Project's Data DB.
+ * This is a working alternative to getDataDB.changes
+ * (as getDataDB.changes that may detach after updates to the owning listing
+ * or the owning active DB, or if the sync is toggled on/off)
+ *
+ * @param active_id Project ID to listen on the DB for.
+ * @param change_opts
+ * @param change_listener
+ * @param error_listener
+ * @returns Detach function: call this to stop all changes
+ */
+export function listenDataDB(
+  active_id: ProjectID,
+  change_opts: PouchDB.Core.ChangesOptions,
+  change_listener: (
+    value: PouchDB.Core.ChangesResponseChange<ProjectDataObject>
+  ) => any,
+  error_listener: (value: any) => any
+): () => void {
+  const current_changes: [null | PouchDB.Core.Changes<ProjectDataObject>] = [
+    null,
+  ];
+
+  const project_update_cb = (
+    type: ['update', createdProjectsInterface] | ['delete'] | ['create'],
+    meta_changed: boolean,
+    data_changed: boolean,
+    listing: unknown,
+    active: ExistingActiveDoc
+  ) => {
+    if (active_id === active._id) {
+      if (data_changed) {
+        // Database has changed, re-attach
+        current_changes[0] = data_dbs[active_id]!.local.changes(change_opts);
+        current_changes[0].on('change', change_listener);
+        current_changes[0].on('error', error_listener);
+      } else if (type[0] === 'delete') {
+        // This is only necessary to avoid all_state_cb throwing an error
+        // When projects are deleted
+        current_changes[0] = null;
+      }
+    }
+  };
+
+  events.on('project_update', project_update_cb);
+
+  /*
+  All state is monitored because, just like getDataDB, when all projects are
+  know and the changes hasn't been set yet, the user has tried to listen on
+  a Data DB that doesn't exist.
+  */
+  const all_state_cb = () => {
+    if (all_projects_updated && current_changes[0] === null) {
+      error_listener(Error(`Project ${active_id} is not known`));
+      detach_cb();
+    }
+  };
+
+  events.on('all_state', all_state_cb);
+
+  const detach_cb = () => {
+    if (current_changes[0] !== null) {
+      current_changes[0].cancel();
+    }
+    events.removeListener('project_update', project_update_cb);
+    events.removeListener('all_state', all_state_cb);
+  };
+
+  return detach_cb;
+}
+
 export async function getProjectDB(
   active_id: ProjectID
 ): Promise<PouchDB.Database<ProjectMetaObject>> {
@@ -70,4 +142,78 @@ export async function getProjectDB(
       throw `Project ${active_id} is not known`;
     }
   }
+}
+
+/**
+ * Allows you to listen for changes from a Project's Meta DB.
+ * This is a working alternative to getProjectDB.changes
+ * (as getProjectDB.changes that may detach after updates to the owning listing
+ * or the owning active DB, or if the sync is toggled on/off)
+ *
+ * @param active_id Project ID to listen on the DB for.
+ * @param change_opts
+ * @param change_listener
+ * @param error_listener
+ * @returns Detach function: call this to stop all changes
+ */
+export function listenProjectDB(
+  active_id: ProjectID,
+  change_opts: PouchDB.Core.ChangesOptions,
+  change_listener: (
+    value: PouchDB.Core.ChangesResponseChange<ProjectMetaObject>
+  ) => any,
+  error_listener: (value: any) => any
+): () => void {
+  const current_changes: [null | PouchDB.Core.Changes<ProjectMetaObject>] = [
+    null,
+  ];
+
+  const project_update_cb = (
+    type: ['update', createdProjectsInterface] | ['delete'] | ['create'],
+    meta_changed: boolean,
+    data_changed: boolean,
+    listing: unknown,
+    active: ExistingActiveDoc
+  ) => {
+    if (active_id === active._id) {
+      if (meta_changed) {
+        // Database has changed, re-attach
+        current_changes[0] = metadata_dbs[active_id]!.local.changes(
+          change_opts
+        );
+        current_changes[0].on('change', change_listener);
+        current_changes[0].on('error', error_listener);
+      } else if (type[0] === 'delete') {
+        // This is only necessary to avoid all_state_cb throwing an error
+        // When projects are deleted
+        current_changes[0] = null;
+      }
+    }
+  };
+
+  events.on('project_update', project_update_cb);
+
+  /*
+  All state is monitored because, just like getProjectDB, when all projects are
+  know and the changes hasn't been set yet, the user has tried to listen on
+  a Meta DB that doesn't exist.
+  */
+  const all_state_cb = () => {
+    if (all_projects_updated && current_changes[0] === null) {
+      error_listener(Error(`Project ${active_id} is not known`));
+      detach_cb();
+    }
+  };
+
+  events.on('all_state', all_state_cb);
+
+  const detach_cb = () => {
+    if (current_changes[0] !== null) {
+      current_changes[0].cancel();
+    }
+    events.removeListener('project_update', project_update_cb);
+    events.removeListener('all_state', all_state_cb);
+  };
+
+  return detach_cb;
 }
