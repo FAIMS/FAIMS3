3ed2364f0caa7ced2b5c5d7a814d1e870bf0048e
katesheng <katesheng@gmail.com>
2021-10-07 15:45:39 +1100

    fix issue
    Signed-off-by: katesheng <katesheng@gmail.com>

--- src/gui/components/project/tabs/TabTab.tsx
+++ src/gui/components/project/tabs/TabTab.tsx
@@ -18,153 +18,160 @@
  *   TODO
  */
 
-
-
 import React from 'react';
-import {Tab,Tabs,Hidden,Grid} from '@material-ui/core';
+import {Tab, Tabs, Hidden, Grid} from '@material-ui/core';
 import Icon from '@material-ui/core/Icon';
-import { makeStyles } from '@material-ui/core/styles';
-import { useState, useEffect } from 'react'
+import {makeStyles} from '@material-ui/core/styles';
+import {useState, useEffect} from 'react';
 import TextField from '@material-ui/core/TextField';
-import {AddSectionButton,EditButton,TickButton} from './ProjectButton';
+import {AddSectionButton, EditButton, TickButton} from './ProjectButton';
 import {FormForm} from '../FormElement';
 import {gettabform} from '../data/ComponentSetting';
 
-
-function a11yProps(tabname:any,index:any) {
+function a11yProps(tabname: any, index: any) {
   return {
     id: `${tabname}-${index}`,
     'aria-controls': `${tabname}panel-${index}`,
   };
 }
 
-const useStyles = makeStyles((theme) => ({
-  fieldtab:{
-    
-    textAlign:'left',
-    minWidth:55,
+const useStyles = makeStyles(theme => ({
+  fieldtab: {
+    textAlign: 'left',
+    minWidth: 55,
     // backgroundColor:'#e1e4e8',
   },
-  subtab:{
+  subtab: {
     // borderTop:'1px solid',
     // borderTopColor:theme.palette.primary.main
   },
-  root:{
-
-  }
+  root: {},
 }));
 
-type TabProps={
-  tabs:Array<string>;
-  tab_id:string;
-  value:number;
-  handleChange:any;
-  handelonChangeLabel?:any;
+type TabProps = {
+  tabs: Array<string>;
+  tab_id: string;
+  value: number;
+  handleChange: any;
+  handelonChangeLabel?: any;
+};
 
-}
-
-export function TabTab(props:TabProps){
+export function TabTab(props: TabProps) {
   const classes = useStyles();
-    const {tabs,tab_id,value,handleChange, ...other} = props
-    return (<Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label={tab_id}
-          id={tab_id}
-          orientation={tab_id==='primarytab'?  "horizontal" :"vertical" }
-      >
-        {tabs.map((tab:any,index:number)=>(
-          <Tab className={tab_id==='primarytab'?  classes.root :classes.fieldtab } key={`${tab_id}-${index}`} 
-          label={tab}  {...a11yProps(tab_id,index)} />  ))}  
-      </Tabs>);
+  const {tabs, tab_id, value, handleChange, ...other} = props;
+  return (
+    <Tabs
+      value={value}
+      onChange={handleChange}
+      aria-label={tab_id}
+      id={tab_id}
+      orientation={tab_id === 'primarytab' ? 'horizontal' : 'vertical'}
+    >
+      {tabs.map((tab: any, index: number) => (
+        <Tab
+          className={tab_id === 'primarytab' ? classes.root : classes.fieldtab}
+          key={`${tab_id}-${index}`}
+          label={tab}
+          {...a11yProps(tab_id, index)}
+        />
+      ))}
+    </Tabs>
+  );
 }
 
 export function TabEditable(props: TabProps) {
   const classes = useStyles();
-  const {tabs,tab_id,value,handleChange, ...other} = props
-  const [tablists,setTablist]=useState<Array<any>>(tabs)
-  const [isedited, setisedited]=useState(false)
-  const [isset,setIsset]=useState(false)
+  const {tabs, tab_id, value, handleChange, ...other} = props;
+  const [tablists, setTablist] = useState<Array<any>>(tabs);
+  const [isedited, setisedited] = useState(false);
+  const [isset, setIsset] = useState(false);
   useEffect(() => {
-     setTablist(tabs);
-    }, [tabs]);
+    setTablist(tabs);
+  }, [tabs]);
 
+  const handleEdit = (event: any) => {
+    setisedited(true);
+  };
+  const handleAdd = (event: any) => {
+    const newtabs = tablists;
+    const length = tablists.length + 1;
+    let name = 'Section';
+    if (tab_id === 'formtab') name = 'Form';
+    newtabs[tablists.length] = name + length;
+    setTablist(newtabs);
+    props.handelonChangeLabel(newtabs, 'add');
+    setIsset(!isset);
+  };
 
-  const handleEdit = (event:any) => {
-      setisedited(true)
-    
-    }
-    const handleAdd =  (event:any) => {
-      const newtabs=tablists;
-      const length=tablists.length+1
-      let name='Section' 
-      if(tab_id==='formtab') name='Form'
-      newtabs[tablists.length]=name+length
-      setTablist(newtabs)
-      props.handelonChangeLabel(newtabs,'add')
-      setIsset(!isset)
-    }
+  const handleSubmit = (event: any) => {
+    console.log(event);
+    setisedited(false);
+  };
 
-    const handleSubmit = (event:any) =>{
-      console.log(event)
-      setisedited(false)
-    }
+  const handleSubmitForm = (values: any) => {
+    const newtabs = tablists;
+    const pretabs = tablists;
+    Object.entries(values).map((value, index) => (newtabs[index] = value[1]));
+    props.handelonChangeLabel(newtabs, 'update');
+    setTablist(newtabs);
+    setisedited(false);
+  };
 
-    const handleSubmitForm = (values:any) =>{
-      
-      
-      const newtabs=tablists
-      const pretabs=tablists
-      Object.entries(values).map((value,index) =>newtabs[index]=value[1] )
-      props.handelonChangeLabel(newtabs,'update')
-      setTablist(newtabs) 
-      setisedited(false)
-      
-    }
+  const handleChangeForm = (event: any) => {
+    // console.log(event.target.name+event.target.value)
+  };
 
-    const handleChangeForm =(event:any) =>{
-      // console.log(event.target.name+event.target.value)
-    }
-
-
-  return tab_id==='primarytab'?(
-      <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label={tab_id}
-          id={tab_id}
-      >
-        {tablists.map((tab,index)=>(
-          <Tab className={classes.root} key={`${tab_id}-${index}`} 
-          label={tab}  {...a11yProps(tab_id,index)} />  ))}  
-      </Tabs>
-    
-  ):( <Grid container className={classes.subtab}>
-            
-          <Grid item sm={11} xs={12}>
-          {isedited===false?
+  return (
+    <Grid container className={classes.subtab}>
+      <Grid item sm={11} xs={12}>
+        {isedited === false ? (
           <Tabs
-              value={value}
-              onChange={handleChange}
-              aria-label={tab_id}
-              id={tab_id}
-              orientation={tab_id==='fieldtab'?  "vertical" :"horizontal"}
-              
+            value={value}
+            onChange={handleChange}
+            aria-label={tab_id}
+            id={tab_id}
+            orientation={tab_id === 'fieldtab' ? 'vertical' : 'horizontal'}
           >
-            {tablists.map((tab,index)=>(
-              <Tab className={tab_id==='fieldtab'? classes.fieldtab: classes.subtab } key={`${tab_id}-${index}`} label={tab}  {...a11yProps({tab_id},{index})}  />))}  
-          </Tabs>:
-          <FormForm uiSpec={gettabform(tabs)} currentView='start-view' handleChangeForm={handleChangeForm} handleSubmit={handleSubmitForm}/>}
-        </Grid>
-        <Grid item sm={1} xs={12}>
-        {isedited===false?<><EditButton onButtonClick={handleEdit} value={1} id='edit' text='X'/>
-          <AddSectionButton onButtonClick={handleAdd} value={1} id='add' text='X'/></>:''}
-          
-          </Grid>
+            {tablists.map((tab, index) => (
+              <Tab
+                className={
+                  tab_id === 'fieldtab' ? classes.fieldtab : classes.subtab
+                }
+                key={`${tab_id}-${index}`}
+                label={tab}
+                {...a11yProps({tab_id}, {index})}
+              />
+            ))}
+          </Tabs>
+        ) : (
+          <FormForm
+            uiSpec={gettabform(tabs)}
+            currentView="start-view"
+            handleChangeForm={handleChangeForm}
+            handleSubmit={handleSubmitForm}
+          />
+        )}
+      </Grid>
+      <Grid item sm={1} xs={12}>
+        {isedited === false ? (
+          <>
+            <EditButton
+              onButtonClick={handleEdit}
+              value={1}
+              id="edit"
+              text="X"
+            />
+            <AddSectionButton
+              onButtonClick={handleAdd}
+              value={1}
+              id="add"
+              text="X"
+            />
+          </>
+        ) : (
+          ''
+        )}
+      </Grid>
     </Grid>
-    
   );
-
 }
-
-
