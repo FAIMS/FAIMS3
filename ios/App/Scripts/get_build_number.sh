dfa70b29064eb693ac569038ba083ee7d8dcc1b9
Brian <brian@192-168-1-102.tpgi.com.au>
2021-11-19 16:57:51 +1100

    pre xcode cleanup
    Signed-off-by: Brian Ballsun-Stanton <denubis@noreply.users.github.com>

--- ios/App/Scripts/get_build_number.sh
+++ ios/App/Scripts/get_build_number.sh
@@ -3,14 +3,14 @@
 
 git=$(sh /etc/profile; which git)
 number_of_commits=$("$git" rev-list HEAD --count)
-git_release_version=$("$git" describe --tags --always --abbrev=0)
+git_release_version=$(git describe --long 2>/dev/null || git describe --all --long --always | sed "s#heads/g#${platform}#")
 
 target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
 dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
 
 for plist in "$target_plist" "$dsym_plist"; do
   if [ -f "$plist" ]; then
-    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $number_of_commits" "$plist"
-    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString ${git_release_version#*v}" "$plist"
+    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $number_of_commits#*v" "$plist"
+    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $number_of_commits#*v}" "$plist"
   fi
 done
