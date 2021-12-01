34ece5166b508a507598e3821d49dae847812e10
riniangreani <rini.angreani@csiro.au>
2021-12-01 22:29:32 +0800

    e2e: scroll to the top of the page to get the "draft last saved" message
    Signed-off-by: riniangreani <rini.angreani@csiro.au>

--- e2e/src/test/java/org/fedarch/faims3/TestIncompleteDraftObservation.java
+++ e2e/src/test/java/org/fedarch/faims3/TestIncompleteDraftObservation.java
@@ -95,6 +95,9 @@ public class TestIncompleteDraftObservation {
 			//Wait 2 seconds but do not click SAVE AND NEW
 		    Thread.sleep(2000);
 
+		    // Scroll to the top
+		    TestUtils.scrollToText(driver, "start-view");
+
 			//Note the blue box at the top of the form notifying
 			//“✔ Draft last saved a few seconds ago DD Mmm dd hh:mm:ss GMT+1000 (AEST)”
 		    WebElement messages = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(
