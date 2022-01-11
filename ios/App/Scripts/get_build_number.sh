ddb16b0d5ba55d12334bdecfae80801da8138fc9
Brian <brian@192-168-1-102.tpgi.com.au>
2022-01-11 12:51:57 +1100

    ok, this one ran to app store, hopefully it works remotely
    Signed-off-by: Brian <brian@192-168-1-102.tpgi.com.au>

--- ios/App/Scripts/get_build_number.sh
+++ ios/App/Scripts/get_build_number.sh
@@ -1,14 +1,27 @@
-#!/bin/bash
-#https://mokacoding.com/blog/automatic-xcode-versioning-with-git/
+#!/bin/bash -euo pipefail
+#https://pgu.dev/2020/12/16/ios-build-versioning.html
+
+#!/bin/sh -euo pipefail
+
+# Convert elapsed seconds from Unix epoch until now to minutes
+# HOURS_SINCE_EPOCH=$(( $(date "+%s")/60/60 ))
+#
+# # Convert current git HEAD commit (7 characters) to decimal value
+# #GIT_HASH_DECIMAL=$(printf "%d" 0x1"$(git rev-parse --short HEAD)")
+#
+# # Merge both values to a single string
+# BUNDLE_VERSION="${HOURS_SINCE_EPOCH}"."${GIT_HASH_DECIMAL}"
+#
+# # Prepare directory / file where the generated value will be written.
+# mkdir -p "${SRCROOT}"/Plist
+# touch "${SRCROOT}"/Plist/Prefix
+#
+# # Write content to a file
+# cat <<EOF > "${SRCROOT}"/Plist/Prefix
+# #define BUNDLE_VERSION ${BUNDLE_VERSION}
+# EOF
+
 
 buildNumber=$(date -u "+%Y%m%d%H%M")
-
-target_plist="$TARGET_BUILD_DIR/$INFOPLIST_PATH"
-dsym_plist="$DWARF_DSYM_FOLDER_PATH/$DWARF_DSYM_FILE_NAME/Contents/Info.plist"
-
-for plist in "$target_plist" "$dsym_plist"; do
-  if [ -f "$plist" ]; then
-    /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber#*v" "$plist"
-    /usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $buildNumber#*v}" "$plist"
-  fi
-done
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" "App/Info.plist"
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString 0.3.0" "App/Info.plist"
--- ios/App/fastlane/README.md
+++ ios/App/fastlane/README.md
@@ -1,39 +1,48 @@
 fastlane documentation
-================
+----
+
 # Installation
 
 Make sure you have the latest version of the Xcode command line tools installed:
 
-```
+```sh
 xcode-select --install
 ```
 
-Install _fastlane_ using
-```
-[sudo] gem install fastlane -NV
-```
-or alternatively using `brew install fastlane`
+For _fastlane_ installation instructions, see [Installing _fastlane_](https://docs.fastlane.tools/#installing-fastlane)
 
 # Available Actions
+
 ## iOS
+
 ### ios test_ipa
+
+```sh
+[bundle exec] fastlane ios test_ipa
 ```
-fastlane ios test_ipa
-```
+
+
 
 ### ios closed_beta_browserstack
-```
-fastlane ios closed_beta_browserstack
+
+```sh
+[bundle exec] fastlane ios closed_beta_browserstack
 ```
 
+
+
 ### ios closed_beta_testflight
+
+```sh
+[bundle exec] fastlane ios closed_beta_testflight
 ```
-fastlane ios closed_beta_testflight
-```
+
 
 
 ----
 
 This README.md is auto-generated and will be re-generated every time [_fastlane_](https://fastlane.tools) is run.
-More information about fastlane can be found on [fastlane.tools](https://fastlane.tools).
-The documentation of fastlane can be found on [docs.fastlane.tools](https://docs.fastlane.tools).
+
+More information about _fastlane_ can be found on [fastlane.tools](https://fastlane.tools).
+
+The documentation of _fastlane_ can be found on [docs.fastlane.tools](https://docs.fastlane.tools).
