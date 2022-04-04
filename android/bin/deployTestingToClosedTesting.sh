0c14bb39bdc7be6e33eb067e76e6fd0d7ad76e47
Brian Ballsun-Stanton <denubis@noreply.users.github.com>
2022-04-04 11:58:56 +1000

    commit
    Signed-off-by: Brian Ballsun-Stanton <denubis@noreply.users.github.com>

--- android/bin/deployTestingToClosedTesting.sh
+++ android/bin/deployTestingToClosedTesting.sh
@@ -20,7 +20,8 @@ git fetch --all
 git switch --detach
 #faims_tag=$(git describe --tags `git rev-list --tags=v* --max-count=1`)    
 git merge main --no-edit
-
+git branch
+git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 
 
 export REACT_APP_COMMIT_VERSION=$(bin/getDescribeString.sh android)
