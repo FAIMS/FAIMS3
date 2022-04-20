4ce3784bb456a5ca349ee7a6e2565a69d27aa554
katesheng <katesheng@gmail.com>
2022-04-20 13:22:32 +1000

    add the attachment sync setting page
    Signed-off-by: katesheng <katesheng@gmail.com>

---
+++ src/gui/pages/project-settingattch.tsx
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2021, 2022 Macquarie University
+ *
+ * Licensed under the Apache License Version 2.0 (the, "License");
+ * you may not use, this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND either express or implied.
+ * See, the License, for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Filename: projectsetting attachment.tsx
+ * Description:
+ *   This file is for user to setup the syncing for attachment/photoes and download the attachement files and photoes
+ * TODO: 
+ *   add the sync attahcment function and download files function
+ */
+
+import React, {useEffect, useState} from 'react';
+import {useParams, Redirect, Link as RouterLink} from 'react-router-dom';
+import {
+  Box,
+  Button,
+  Container,
+  Typography,
+  Paper,
+  CircularProgress,
+  TextareaAutosize,
+  Switch, FormControlLabel,
+} from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+
+import Breadcrumbs from '../components/ui/breadcrumbs';
+import * as ROUTES from '../../constants/routes';
+
+import {getProjectInfo, listenProjectInfo} from '../../databaseAccess';
+import {useEventedPromise, constantArgsShared} from '../pouchHook';
+import {ProjectInformation} from '../../datamodel/ui';
+import {dumpMetadataDBContents} from '../../uiSpecification';
+import {ProjectID} from '../../datamodel/core';
+import {TokenContents} from '../../datamodel/core';
+type ProjectProps = {
+  token?: null | undefined | TokenContents;
+};
+
+export default function PROJECTATTACHMENT(props: ProjectProps) {
+  const {project_id} = useParams<{project_id: ProjectID}>();
+
+  // TODO: remove these once we can send new project up
+  const [loading, setLoading] = useState(true);
+  const [metadbContents, setMetadbContents] = useState<object[]>([]);
+  const [isSyncing,setIsSync]=useState(false);
+  const [donloadedattachment,setdonloadedattachment]=useState<object[]>([{key:1}]);
+
+  let project_info: ProjectInformation | null;
+  try {
+    project_info = useEventedPromise(
+      getProjectInfo,
+      constantArgsShared(listenProjectInfo, project_id),
+      false,
+      [project_id],
+      project_id
+    ).expect();
+  } catch (err: any) {
+    if (err.message === 'missing') {
+      return <Redirect to="/404" />;
+    } else {
+      throw err;
+    }
+  }
+
+  const breadcrumbs = [
+    {link: ROUTES.HOME, title: 'Home'},
+    {link: ROUTES.PROJECT_LIST, title: 'Notebook'},
+    {
+      link: ROUTES.PROJECT + project_id,
+      title: project_info !== null ? project_info.name : '[loading]',
+    },
+    {title: 'Attachment'},
+  ];
+
+  useEffect(() => {
+    if (project_id === null) return;
+    const getDB = async () => {
+      setMetadbContents(await dumpMetadataDBContents(project_id));
+      setLoading(false);
+    };
+    getDB();
+  }, []);
+
+
+  return project_info ? (
+    <Container maxWidth="lg">
+      <Breadcrumbs data={breadcrumbs} token={props.token} />
+
+      <Box mb={2}>
+        <Typography variant={'h2'} component={'h1'}>
+          {project_info.name} Attachment Settings
+        </Typography>
+        <Typography variant={'subtitle1'} gutterBottom>
+          Update the project Attachment settings for{' '}
+          {project_info.name}.
+        </Typography>
+      </Box>
+      <Paper square>
+        <span>Attachment is auto Sync(Attachment and photoes will be auto downloaded if enabled)</span>
+        <FormControlLabel
+          control={
+            <Switch
+              disabled={loading}
+              checked={isSyncing}
+              onChange={(event) =>
+                {setIsSync(!isSyncing)
+                console.log(isSyncing)}
+              }
+            />
+          }
+          label={<Typography variant={'button'}>Sync</Typography>}
+        />
+        <br/>
+         <Button
+          color="primary"
+          size="large"
+          variant="contained"
+          onClick={(event) =>
+            {alert('downloading')}
+          }
+        >
+          Donwload all Attachment and Images
+        </Button>
+        <br/>
+        <br/>
+        Downloaded Attachments: 
+        <ul>
+        {donloadedattachment.map((attach,index)=>(<li key={index} >{index}</li>))}
+        </ul>
+      </Paper>
+      
+    </Container>
+  ) : (
+    <CircularProgress />
+  );
+}
