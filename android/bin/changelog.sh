cf7353cfa7c25eaf2008bef8c8a67d112ae65305
Brian Ballsun-Stanton <Denubis@users.noreply.github.com>
2022-07-12 14:54:32 +1000

    more changelog fixing while I notice it
    Signed-off-by: Brian Ballsun-Stanton <Denubis@users.noreply.github.com>

--- android/bin/changelog.sh
+++ android/bin/changelog.sh
@@ -3,6 +3,7 @@
 GITROOT=$(git rev-parse --show-toplevel)
 cd $GITROOT
 
+mkdir -p $GITROOT/android/metadata/android/en-GB/changelogs/
 git log --perl-regexp --pretty="* %s" | awk '//{f=1}; f && g<2; $0 && /Merge remote/{g++}' | grep -v "Merge remote" > $GITROOT/android/metadata/android/en-GB/changelogs/$1.txt
 count=$(wc -c $GITROOT/android/metadata/android/en-GB/changelogs/$1.txt | cut -d' ' -f1)
 echo $count
