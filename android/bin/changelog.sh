43fb1527157274e23274632c8f6553d60c03fffd
Brian Ballsun-Stanton <Denubis@users.noreply.github.com>
2022-07-12 13:49:00 +1000

    fixing changelog.sh
    Signed-off-by: Brian Ballsun-Stanton <Denubis@users.noreply.github.com>

--- android/bin/changelog.sh
+++ android/bin/changelog.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-git log --perl-regexp --pretty="* %s" | awk '//{f=1}; f && g<2; $0 && /Merge remote/{g++}' | grep -v "Merge remote" > ./metadata/android/en-GB/changelogs/$1.txt
-count=$(wc -c ./metadata/android/en-GB/changelogs/$1.txt | cut -d' ' -f1)
+GITROOT=$(git rev-parse --show-toplevel)
+cd $GITROOT
+
+git log --perl-regexp --pretty="* %s" | awk '//{f=1}; f && g<2; $0 && /Merge remote/{g++}' | grep -v "Merge remote" > $GITROOT/android/metadata/android/en-GB/changelogs/$1.txt
+count=$(wc -c $GITROOT/android/metadata/android/en-GB/changelogs/$1.txt | cut -d' ' -f1)
 echo $count
 if [[ $count -gt 497 ]]
 then
-	sed -Ez -i -- 's/^(.{497}).*/\1.../' ./metadata/android/en-GB/changelogs/$1.txt
+	sed -Ez -i -- 's/^(.{497}).*/\1.../' $GITROOT/android/metadata/android/en-GB/changelogs/$1.txt
 fi
