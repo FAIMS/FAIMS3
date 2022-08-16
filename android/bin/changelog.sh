ac40472b6263b5605c2004d6b832d81038cc1001
Brian Ballsun-Stanton <denubis@noreply.users.github.com>
2022-08-16 10:39:02 +1000

    Fastlane changes for fieldmark
    Signed-off-by: Brian Ballsun-Stanton <denubis@noreply.users.github.com>

--- android/bin/changelog.sh
+++ android/bin/changelog.sh
@@ -3,11 +3,11 @@
 GITROOT=$(git rev-parse --show-toplevel)
 cd $GITROOT
 
-mkdir -p $GITROOT/android/metadata/android/en-GB/changelogs/
-git log --perl-regexp --pretty="* %s" | awk '//{f=1}; f && g<2; $0 && /Merge remote/{g++}' | grep -v "Merge remote" > $GITROOT/android/metadata/android/en-GB/changelogs/$1.txt
-count=$(wc -c $GITROOT/android/metadata/android/en-GB/changelogs/$1.txt | cut -d' ' -f1)
+mkdir -p $GITROOT/android/fastlane/metadata/android/en-GB/changelogs/
+git log --perl-regexp --pretty="* %s" | awk '//{f=1}; f && g<2; $0 && /Merge remote/{g++}' | grep -v "Merge remote" > $GITROOT/android/fastlane/metadata/android/en-GB/changelogs/$1.txt
+count=$(wc -c $GITROOT/android/fastlane/metadata/android/en-GB/changelogs/$1.txt | cut -d' ' -f1)
 echo $count
 if [[ $count -gt 497 ]]
 then
-	sed -Ez -i -- 's/^(.{497}).*/\1.../' $GITROOT/android/metadata/android/en-GB/changelogs/$1.txt
+	sed -Ez -i -- 's/^(.{497}).*/\1.../' $GITROOT/android/fastlane/metadata/android/en-GB/changelogs/$1.txt
 fi
