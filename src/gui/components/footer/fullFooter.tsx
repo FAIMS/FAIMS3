75a29ad77d8359a12924fe7b08391b38d6734414
Liz Mannering <elizabeth.mannering@mq.edu.au>
2022-09-02 12:27:10 +0800

    feat: footer update to logos (colour) and alignment feat: change theme to include faims orange as secondary colour
    FAIMS3-550 FAIMS3-549
    
    Signed-off-by: Liz Mannering <elizabeth.mannering@mq.edu.au>

--- src/gui/components/footer/fullFooter.tsx
+++ src/gui/components/footer/fullFooter.tsx
@@ -24,7 +24,7 @@ export default function FullFooter(props: FullFooterProps) {
         <Grid item xs={12} sx={{display: {xs: 'block', sm: 'none'}}}>
           <SlimFooter token={props.token} />
         </Grid>
-        <Grid item sm={4} sx={{display: {xs: 'none', sm: 'block'}}}>
+        <Grid item sm={4} md={4} lg={3} sx={{display: {xs: 'none', sm: 'block'}, mt: 1}}>
           <Box>
             <img
               src="/static/logo/Faims-small.png"
@@ -41,7 +41,7 @@ export default function FullFooter(props: FullFooterProps) {
             </Typography>
           </Box>
         </Grid>
-        <Grid item sm={4} sx={{display: {xs: 'none', sm: 'block'}}}>
+        <Grid item sm={5} md={5} lg={3} sx={{display: {xs: 'none', sm: 'block'}, mt: 1}}>
           <Typography
             variant="overline"
             display="block"
@@ -55,12 +55,13 @@ export default function FullFooter(props: FullFooterProps) {
             through to archiving for any research discipline, workflow,
             ontology, and standard.
           </Typography>
-          <br />
+        </Grid>
+        <Grid item sm={3} md={3} lg={3} sx={{display: {xs: 'none', sm: 'block'}, mt: 1}}>
           <Typography
             variant="overline"
             display="block"
             gutterBottom
-            sx={{color: theme.palette.grey[600], mt: 1}}
+            sx={{color: theme.palette.grey[600]}}
           >
             Support
           </Typography>
@@ -98,7 +99,7 @@ export default function FullFooter(props: FullFooterProps) {
             <br />
           </Typography>
         </Grid>
-        <Grid item sm={4} sx={{display: {xs: 'none', sm: 'block'}}}>
+        <Grid item sm={12} md={12} lg={3} sx={{display: {xs: 'none', sm: 'block'}, mt: 1}}>
           <Typography
             variant="overline"
             display="block"
@@ -107,79 +108,79 @@ export default function FullFooter(props: FullFooterProps) {
           >
             Partner Organisations
           </Typography>
-          <Box>
-            <img
-              src={'/static/logo/partners/ARDC_logo_RGB.png'}
-              alt={'ardc logo'}
-              style={{
-                maxWidth: '150px',
-                width: '100%',
-                filter: 'grayscale(100%)',
-              }}
-            />
-            <br />
-            <Typography variant={'caption'}>
-              The FAIMS 3.0 Electronic Field Notebooks project received
-              investment (doi: 10.47486/PL110) from the Australian Research Data
-              Commons (ARDC). The ARDC is funded by the National Collaborative
-              Research Infrastructure Strategy (NCRIS).
-            </Typography>
-          </Box>
+          <Grid container spacing={2}>
+            <Grid item>
+              <img
+                  src={'/static/logo/partners/ARDC_logo_RGB.png'}
+                  alt={'ardc logo'}
+                  style={{
+                    maxWidth: '150px',
+                    width: '100%',
+                    marginTop:'10px'
+                  }}
+              />
+            </Grid>
+            <Grid item  sm={8} lg={12}>
+              <Typography variant={'caption'}>
+                The FAIMS 3.0 Electronic Field Notebooks project received
+                investment (doi: 10.47486/PL110) from the Australian Research Data
+                Commons (ARDC). The ARDC is funded by the National Collaborative
+                Research Infrastructure Strategy (NCRIS).
+              </Typography>
+            </Grid>
+
+
+          </Grid>
           <Divider sx={{my: 1}} />
           <Box>
-            <Grid container justifyContent={'center'} alignItems="center">
+            <Grid container >
               <Grid
                 item
-                xs={12}
-                sm={12}
-                md={12}
-                lg={8}
                 style={{textAlign: 'left'}}
               >
                 <img
-                  src={'/static/logo/partners/MQ_INT_HOR_BLACK.png'}
+                  src={'/static/logo/partners/MQ_INT_HOR_RGB_POS.png'}
                   alt={'macquarie university logo'}
                   style={{
                     maxWidth: '200px',
                     width: '100%',
-                    filter: 'grayscale(100%)',
                   }}
                 />
               </Grid>
               <Grid
                 item
-                xs={12}
-                sm={12}
-                md={12}
-                lg={4}
                 style={{textAlign: 'left'}}
               >
                 <img
-                  src={'/static/logo/partners/CSIRO_Black.png'}
+                  src={'/static/logo/partners/CSIRO_Solid_RGB.png'}
                   alt={'csiro logo'}
                   style={{
-                    maxWidth: '60px',
+                    maxWidth: '80px',
                     width: '100%',
-                    filter: 'grayscale(100%)',
                     opacity: 0.8,
+                    paddingLeft: theme.spacing(2),
+                    paddingTop: theme.spacing(1),
                   }}
                 />
               </Grid>
             </Grid>
+            <Box sx={{textAlign:'right'}}>
+              <Link
+                  href={
+                      process.env.REACT_APP_PARTNERS_HREF ??
+                      'https://faims.edu.au/partners/'
+                  }
+                  variant="caption"
+                  target="_blank"
+                  rel="noreferrer"
+                  sx={{textDecoration: 'none', color: theme.palette.text.primary}}
+              >
+                All FAIMS partners
+              </Link>
+            </Box>
           </Box>
-          <Divider />
-          <Link
-            href={
-              process.env.REACT_APP_PARTNERS_HREF ??
-              'https://faims.edu.au/partners/'
-            }
-            variant="caption"
-            target="_blank"
-            rel="noreferrer"
-            sx={{textDecoration: 'none', color: theme.palette.text.primary}}
-          >
-            All FAIMS partners
-          </Link>
+
+
         </Grid>
       </Grid>
     </Box>
