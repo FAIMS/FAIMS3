5cdadfe2f1e027722058d2a2122c777fad99ad42
Liz Mannering <elizabeth.mannering@mq.edu.au>
2022-09-19 18:05:37 +0800

    feat: notebook list view sorted alphabetically by default (but user can sort themselves by e.g., last_updated). FAIMS3-571
    Signed-off-by: Liz Mannering <elizabeth.mannering@mq.edu.au>

---
+++ src/gui/components/ui/circular_loading.tsx
@@ -0,0 +1,23 @@
+import {CircularProgress, Grid, Typography} from '@mui/material';
+import React from 'react';
+
+export default function CircularLoading(props: {label: string}) {
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="flex-start"
+      alignItems="center"
+      spacing={1}
+    >
+      <Grid item>
+        <CircularProgress size={14} thickness={5} />
+      </Grid>
+      <Grid item>
+        <Typography variant="body2" color="text.secondary">
+            {props.label}
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+}
--- src/gui/pages/notebook_list.tsx
+++ src/gui/pages/notebook_list.tsx
@@ -18,56 +18,124 @@
  *   TODO
  */
 
-import React from 'react';
-import {Box} from '@mui/material';
+import React, {useEffect, useState} from 'react';
+import {Link as RouterLink, useHistory} from 'react-router-dom';
+import {Box, Paper, Typography, Grid, Alert} from '@mui/material';
+import FolderIcon from '@mui/icons-material/Folder';
+
+import {
+  DataGrid,
+  GridColDef,
+  GridCellParams,
+  GridEventListener,
+} from '@mui/x-data-grid';
 import Breadcrumbs from '../components/ui/breadcrumbs';
 import ProjectCard from '../components/project/card';
 import * as ROUTES from '../../constants/routes';
 import {getProjectList, listenProjectList} from '../../databaseAccess';
-import {CircularProgress} from '@mui/material';
 import {useEventedPromise} from '../pouchHook';
 import {TokenContents} from '../../datamodel/core';
+import {ProjectInformation} from '../../datamodel/ui';
+import CircularLoading from '../components/ui/circular_loading';
 
-type ProjectProps = {
+type NoteBookListProps = {
   token?: null | undefined | TokenContents;
 };
 
-export default function NoteBookList(props: ProjectProps) {
+export default function NoteBookList(props: NoteBookListProps) {
+  const [loading, setLoading] = useState<boolean>(false);
+  const history = useHistory();
   const pouchProjectList = useEventedPromise(
     getProjectList,
     listenProjectList,
     true,
     []
   ).expect();
+
   const breadcrumbs = [
     {link: ROUTES.INDEX, title: 'Home'},
     {title: 'Notebooks'},
+  ];
+  const handleRowClick: GridEventListener<'rowClick'> = params => {
+    history.push(ROUTES.NOTEBOOK + params.row.project_id);
+  };
+  const columns: GridColDef[] = [
+    {
+      field: 'name',
+      headerName: 'Name',
+      type: 'string',
+      flex: 0.5,
+      renderCell: (params: GridCellParams) => (
+        <Box my={1}>
+          <Typography variant={'h5'} sx={{m: 0}} component={'div'}>
+            <Grid
+              container
+              justifyContent="flex-start"
+              alignItems="center"
+              spacing={1}
+            >
+              <Grid item>
+                <FolderIcon
+                  color={'secondary'}
+                  style={{verticalAlign: 'middle'}}
+                />
+              </Grid>
+              <Grid item>{params.row.name}</Grid>
+            </Grid>
+          </Typography>
+          <Typography variant={'caption'}>{params.row.description}</Typography>
+        </Box>
+      ),
+    },
+    {
+      field: 'last_updated',
+      headerName: 'Last Updated',
+      type: 'string',
+      flex: 0.2,
+    },
+    {
+      field: 'created',
+      headerName: 'Created',
+      type: 'string',
+      flex: 0.2,
+    },
+    {
+      field: 'status',
+      headerName: 'Status',
+      type: 'string',
+      flex: 0.1,
+    },
   ];
 
   return (
     <Box>
       <Breadcrumbs data={breadcrumbs} />
       {pouchProjectList === null ? (
-        <CircularProgress />
+        <CircularLoading label={'Loading notebooks'} />
       ) : Object.keys(pouchProjectList).length === 0 ? (
-        <span>No notebooks found</span>
+        <Alert severity={'info'}>No notebooks found</Alert>
       ) : (
-        pouchProjectList.map(project_info => {
-          const project_id = project_info.project_id;
-          if (project_info !== null) {
-            return (
-              <Box key={'project-list' + project_id}>
-                <ProjectCard project={project_info} dashboard={true} />
-              </Box>
-            );
-          } else {
-            return (
-              <Box key={'project-list' + project_id}>
-                Project could not be loaded
-              </Box>
-            );
-          }
-        })
+        <Box component={Paper} elevation={0}>
+          <DataGrid
+            key={'notebook_list_datagrid'}
+            rows={pouchProjectList}
+            loading={loading}
+            columns={columns}
+            onRowClick={handleRowClick}
+            autoHeight
+            getRowId={r => r.project_id}
+            hideFooter={true}
+            getRowHeight={() => 'auto'}
+            initialState={{
+              sorting: {
+                sortModel: [{field: 'name', sort: 'asc'}],
+              },
+              pagination: {
+                pageSize: pouchProjectList.length,
+              },
+            }}
+          />
+        </Box>
       )}
     </Box>
   );
