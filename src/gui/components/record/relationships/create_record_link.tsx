1e45153086bb65ead36c93566db4387ec07561e9
Liz Mannering <elizabeth.mannering@mq.edu.au>
2022-09-27 16:44:09 +0800

    feat: interface for adding new relationships to a record. This needs hooking up
    FAIMS3-553
    
    Signed-off-by: Liz Mannering <elizabeth.mannering@mq.edu.au>

---
+++ src/gui/components/record/relationships/create_record_link.tsx
@@ -0,0 +1,114 @@
+import React from 'react';
+import {
+  Button,
+  Divider,
+  Grid,
+  Typography,
+  TextField,
+  Autocomplete,
+  Box,
+  SelectChangeEvent,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+} from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
+
+export interface RelationshipType {
+  link: string;
+  reciprocal: string;
+}
+interface CreateRecordLinkProps {
+  relationships: Array<RelationshipType>;
+}
+export function CreateRecordLink(props: CreateRecordLinkProps) {
+  /**
+   * Allow users to add a link to a record from the current record
+   */
+  const [relationship, setRelationship] = React.useState('');
+  const [submitting, setSubmitting] = React.useState(false);
+
+  const options = ['Record 1', 'Record 2'];
+  const [value, setValue] = React.useState<string | null>(options[0]);
+  const [inputValue, setInputValue] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setRelationship(event.target.value as string);
+  };
+  const handleSubmit = () => {
+    /**
+     * Submit relationship // TODO
+     */
+    setSubmitting(true);
+    const timer = setTimeout(() => setSubmitting(false), 3000);
+    return () => {
+      clearTimeout(timer);
+    };
+  };
+
+
+  return (
+    <Grid container spacing={1} direction="row" justifyContent="space-between">
+      <Grid item xs={12} sm={5} md={4}>
+        <FormControl fullWidth>
+          <InputLabel id="demo-simple-select-label">Relationship</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="create-record-relationship-type"
+            value={relationship}
+            label="Relationship"
+            onChange={handleChange}
+          >
+            {props.relationships.map(r => (
+              <MenuItem value={r.link} key={r.link}>{r.link}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} sm={5} md={6}>
+        <Autocomplete
+          fullWidth
+          value={value}
+          onChange={(event: any, newValue: string | null) => {
+            setValue(newValue);
+          }}
+          inputValue={inputValue}
+          onInputChange={(event, newInputValue) => {
+            setInputValue(newInputValue);
+          }}
+          id="create-record-relationship-records"
+          options={options}
+          renderInput={params => <TextField {...params} label="Record" />}
+        />
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        sm={2}
+        md={2}
+        alignItems="stretch"
+        style={{display: 'flex'}}
+      >
+        {submitting ? (
+          <LoadingButton
+            loading
+            variant={'contained'}
+            fullWidth
+            size={'large'}
+          />
+        ) : (
+          <Button
+            variant={'contained'}
+            disableElevation
+            fullWidth
+            size={'large'}
+            onClick={handleSubmit}
+          >
+            Add
+          </Button>
+        )}
+      </Grid>
+    </Grid>
+  );
+}
