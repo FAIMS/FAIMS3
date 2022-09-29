bb0cc46ba695bae0a95dec815d38815118eaee4d
Steve Cassidy <steve.cassidy@mq.edu.au>
2022-09-29 11:47:31 +1000

    build on android, add demo qr display

--- src/gui/fields/qrcode/QRCodeFormFieldSettings.tsx
+++ src/gui/fields/qrcode/QRCodeFormFieldSettings.tsx
@@ -18,28 +18,7 @@
  *   Implement Settings widget for QRCodeFormField
  */
 
-// should really be imported from the main project
-interface ProjectUIModel {
-  _id?: string; // optional as we may want to include the raw json in places
-  _rev?: string; // optional as we may want to include the raw json in places
-  fields: {[key: string]: any};
-  views: {
-    [key: string]: {
-      label?: string;
-      fields: string[];
-      uidesign?: string;
-      next_label?: string;
-    };
-  };
-  viewsets: {
-    [type: string]: {
-      label?: string;
-      views: string[];
-      submit_label?: string;
-    };
-  };
-  visible_types: string[];
-}
+import {ProjectUIModel} from '../../../datamodel/ui';
 
 export const QRCodeFieldUISpec = {
   'component-namespace': 'qrcode', // this says what web component to use to render/acquire value from
