3c26dfdd0787458c6c08a7704083f3c75141945b
Brian Ballsun-Stanton <Denubis@users.noreply.github.com>
2022-09-29 10:42:25 +1000

    adding demo to server_configs
    Signed-off-by: Brian Ballsun-Stanton <Denubis@users.noreply.github.com>

---
+++ server_configs/demo.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# invoke dev mode with environment vairables:
+# source server_configs/dev.sh; npm start
+
+export REACT_APP_DIRECTORY_HOST=demo.db.faims.edu.au
+export REACT_APP_DIRECTORY_PORT=443
+export REACT_APP_USE_HTTPS=true
+export REACT_APP_PRODUCTION_BUILD=true
+export REACT_APP_USE_REAL_DATA=true
+export REACT_APP_TAG=Beta
+export REACT_APP_SERVER=production
+export REACT_APP_SERVICES=FAIMSTEXT
