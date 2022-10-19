b92d8d2e903e126a7179ca3b642b66b0f777c63f
Liz Mannering <elizabeth.mannering@mq.edu.au>
2022-10-19 11:54:38 +0800

    feat: UGC report interface. Includes modal to capture report. Report flagging functionality exists at the bottom of a record. fix: error on incorrect nesting of nodes in record_link
    FAIMS3-548
    
    Signed-off-by: Liz Mannering <elizabeth.mannering@mq.edu.au>

--- src/gui/components/record/form.tsx
+++ src/gui/components/record/form.tsx
@@ -23,7 +23,7 @@ import {withRouter} from 'react-router-dom';
 import {RouteComponentProps} from 'react-router';
 import {Formik, Form} from 'formik';
 
-import {Grid, Box, Typography} from '@mui/material';
+import {Grid, Box, Typography, Divider} from '@mui/material';
 
 import {firstDefinedFromList} from './helpers';
 import {get_logic_fields, get_logic_views} from './branchingLogic';
@@ -972,13 +972,14 @@ class RecordForm extends React.Component<
               }}
             </Formik>
           </div>
-          <div>
+          <Box mt={3}>
+            <Divider />
             <UGCReport
-              handleUGCReport={(event: any) => {
-                this.setState({ugc_comment: event.target.value});
+              handleUGCReport={(value: string) => {
+                this.setState({ugc_comment: value});
               }}
             />
-          </div>
+          </Box>
         </Box>
       );
     } else {
