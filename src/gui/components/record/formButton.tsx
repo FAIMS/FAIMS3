3801d674080b72e1bcfea9a6909f0513608651d3
katesheng <katesheng@gmail.com>
2022-10-21 17:26:47 +1100

    remove revision id in form
    Signed-off-by: katesheng <katesheng@gmail.com>

--- src/gui/components/record/formButton.tsx
+++ src/gui/components/record/formButton.tsx
@@ -13,7 +13,7 @@
  * See, the License, for the specific language governing permissions and
  * limitations under the License.
  *
- * Filename: formBuuton.tsx
+ * Filename: formButton.tsx
  * Description:
  *   Form Button:
  *  - Continue
