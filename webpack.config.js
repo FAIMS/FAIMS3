2e4c8f61e78e3c1cc6db7093ba1ff53dd1d3a24d
sajjad111zare <132220099+sajjad111zare@users.noreply.github.com>
2023-05-07 11:03:26 +0430

    Configure Webpack and reinstall packages
    Signed-off-by: Ruslan Kazakov <ruslan.kazakov@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,40 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: ['core.js', '/src/index.tsx'],
-  output: {path: path.resolve(__dirname, 'dist')},
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
-        },
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-    fallback: {
-      util: false,
-    },
+  // stats: {
+  //   // Configure the console output
+  //   errorDetails: true, //this does show errors
+  //   colors: false,
+  //   modules: true,
+  //   reasons: true,
+  // },
+  entry: './src/index.tsx',
+  output: {
+    path: path.join(__dirname, '/dist'), // the bundle output path
+    filename: 'bundle.js', // the name of the bundle
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './src/index.html', // to import index.html file inside index.js
     }),
   ],
+  devServer: {
+    port: 3030, // you can change the port
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/, // .js and .jsx files
+        exclude: /node_modules/, // excluding the node_modules folder
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };
