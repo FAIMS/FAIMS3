520c07748705011b9bda8d543bcfa0be44803005
Steve Cassidy <steve.cassidy@mq.edu.au>
2023-09-11 13:23:15 +1000

    define the global object during development
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+// vite does not shim the global object and we need it for dev but
+// not for the production build
+const global = process.env.NODE_ENV === 'development' ? 'window' : 'global';
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
   define: {
-    //global: 'window',
+    global: global,
     'process.env': {},
   }
 });
