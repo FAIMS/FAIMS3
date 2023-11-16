d2e944a8b0cf9355cbe63f73fd32f756f9cefa56
Steve Cassidy <steve.cassidy@mq.edu.au>
2023-11-16 14:15:49 +1100

    fix camera in browser
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,9 @@ export default defineConfig({
     global: global,
     'process.env': {} /* some libraries check this */,
   },
+  optimizeDeps: {
+    exclude: ['@ionic/pwa-elements'],
+  },
   test: {
     globals: true,
     environment: 'jsdom',
