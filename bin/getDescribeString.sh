0eb112137a64fe23573ddebad5b303578393e4d5
Steve Cassidy <steve.cassidy@mq.edu.au>
2023-12-01 22:26:33 +1100

    put commit hash back into version string
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- bin/getDescribeString.sh
+++ bin/getDescribeString.sh
@@ -5,6 +5,9 @@ if [ -z "$1" ]; then
 else
 	platform="$1";
 fi
+# get some git commit info
+descstring=$(echo $(git describe --long 2>/dev/null || git describe --all --long --always)| tr -d "\n")
 # get the version number from package.json
 version=$(grep '"version":' package.json | cut -d: -f 2 | sed -e 's/[", ]//g')
-echo v$version-$platform
+# put them together in a nice format
+echo "$descstring" | sed -E "s#heads/[A-Za-z0-9-]+-0-g#v${version}-${platform}-#" | sed -E "s#-v.*-g#-#"
