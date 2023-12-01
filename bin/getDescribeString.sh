cdf62871b46725d3c9d990cf47e809a63ee53032
Steve Cassidy <steve.cassidy@mq.edu.au>
2023-12-01 13:16:25 +1100

    use tag name as main version identifier
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- bin/getDescribeString.sh
+++ bin/getDescribeString.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env sh
 
-
-
 if [ -z "$1" ]; then
 	platform="web"
 else
 	platform="$1";
 fi
 
-descstring=$(echo 0.7.$(git log --perl-regexp --pretty='%s' | grep 'Merge #' | head -n1 | cut -d'#' -f2)-$(git describe --long 2>/dev/null || git describe --all --long --always)| tr -d "\n")
-#echo "$descstring"
-echo "$descstring" | sed -E "s#heads/[A-Za-z0-9-]+-0-g#${platform}.#" | sed -E "s#-v.*-g#-#"
-
+# use the most recent tag as the main version descriptor, add the platform
+echo $(git describe --tags --abbrev=0)-"$platform"
