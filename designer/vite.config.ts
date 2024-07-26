cb47167507ec2b2f6d5011c42f12f2411bde8367
Steve Cassidy <steve.cassidy@mq.edu.au>
2024-07-26 13:18:24 +1000

    Ensure different known ports for designer and app
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- designer/vite.config.ts
+++ designer/vite.config.ts
@@ -1,5 +1,23 @@
+/*
+ * Copyright 2021, 2022 Macquarie University
+ *
+ * Licensed under the Apache License Version 2.0 (the, "License");
+ * you may not use, this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND either express or implied.
+ * See, the License, for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Filename: vite.config.ts
+ * Description:
+ *   Configuration for Vite build
+ */
 /// <reference types="vitest" />
-/// <reference types="vite/client" />
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
@@ -9,6 +27,9 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build',
+  },
+  server: {
+    port: 5010,
   },
   test: {
     globals: true,
