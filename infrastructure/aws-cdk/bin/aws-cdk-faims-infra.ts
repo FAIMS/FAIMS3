2c8b6b6574d3e17d1bbe93c198895a85371fd563
Peter Baker <peter.baker122@csiro.au>
2024-07-26 12:35:23 +1000

    Config approach to dispatch from json config file with zod, specify config file name with env variable
    Signed-off-by: Peter Baker <peter.baker122@csiro.au>

--- infrastructure/aws-cdk/bin/aws-cdk-faims-infra.ts
+++ infrastructure/aws-cdk/bin/aws-cdk-faims-infra.ts
@@ -3,30 +3,37 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { HostedZoneAttributes } from "aws-cdk-lib/aws-route53";
 import { FaimsInfraStack } from "../lib/faims-infra-stack";
+import { loadConfig } from "../configs/config-class";
+import * as path from "path";
+
+// Read the config file path from the environment variable
+const configFileName = process.env.CONFIG_FILE_NAME;
+if (!configFileName) {
+  throw new Error("CONFIG_FILE_NAME environment variable is not set");
+}
+
+const configFilePath = path.resolve(__dirname, "../configs", configFileName);
+
+// Load and validate the configuration
+const config = loadConfig(configFilePath);
 
 const app = new cdk.App();
 
-// hz to target
+// Set up hosted zone attributes
 const zoneAttributes: HostedZoneAttributes = {
-  hostedZoneId: "TODO",
-  zoneName: "TODO",
+  hostedZoneId: config.hostedZone.id,
+  zoneName: config.hostedZone.name,
 };
-const primaryCertArn =
-  "TODO";
-const cloudfrontCertArn =
-  "TODO";
 
+// Create the stack
 new FaimsInfraStack(app, "FaimsInfraStack", {
-  // Target ap-southeast-2 for nbic-4 account with ID below
   env: {
-    account: "TODO",
-    region: "TODO",
+    account: config.aws.account,
+    region: config.aws.region,
   },
   hzAttributes: zoneAttributes,
-  primaryCertArn: primaryCertArn,
-  cloudfrontCertArn: cloudfrontCertArn,
-  privateKeySecretArn:
-    "TODO",
-  publicKeySecretArn:
-    "TODO",
+  primaryCertArn: config.certificates.primary,
+  cloudfrontCertArn: config.certificates.cloudfront,
+  privateKeySecretArn: config.secrets.privateKey,
+  publicKeySecretArn: config.secrets.publicKey,
 });
