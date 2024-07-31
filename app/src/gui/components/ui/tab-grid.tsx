8aa4076d482bd467440e4dce4326e6e1a8c4aad3
Luke McFarlane <luke.mcfarlane@rocketlab.com.au>
2024-08-01 09:31:10 +1000

    pulled tab-grid into separate component, created heading-grid component, added config, updated notebooks.tsx to use config
    Signed-off-by: Luke McFarlane <luke.mcfarlane@rocketlab.com.au>

---
+++ app/src/gui/components/ui/tab-grid.tsx
@@ -0,0 +1,135 @@
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import {Box, Stack, Tab} from '@mui/material';
+import {DataGrid, GridEventListener} from '@mui/x-data-grid';
+import {ProjectInformation} from 'faims3-datamodel/build/src/types';
+
+export default function TabGrid({
+  pouchProjectList,
+  tabID,
+  handleChange,
+  handleRowClick,
+  loading,
+  columns,
+  sortModel,
+}: {
+  pouchProjectList: ProjectInformation[];
+  tabID: string;
+  handleChange: (event: React.SyntheticEvent, newValue: string) => void;
+  handleRowClick: GridEventListener<'rowClick'>;
+  loading: boolean;
+  columns: any;
+  sortModel: any;
+}) {
+  return (
+    <TabContext
+      value={
+        pouchProjectList.filter(r => r.is_activated).length === 0 ? '2' : tabID
+      }
+    >
+      <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+        <TabList onChange={handleChange} aria-label="tablist">
+          <Tab
+            label={
+              'Activated (' +
+              pouchProjectList.filter(r => r.is_activated).length +
+              ')'
+            }
+            value="1"
+            disabled={
+              pouchProjectList.filter(r => r.is_activated).length === 0
+                ? true
+                : false
+            }
+          />
+          <Tab
+            label={
+              'Available (' +
+              pouchProjectList.filter(r => !r.is_activated).length +
+              ')'
+            }
+            value="2"
+          />
+        </TabList>
+      </Box>
+      <TabPanel value="1" sx={{px: 0}}>
+        <div style={{display: 'flex', height: '100%'}}>
+          <div style={{flexGrow: 1}}>
+            <DataGrid
+              key={'notebook_list_datagrid'}
+              rows={pouchProjectList.filter(r => r.is_activated)}
+              loading={loading}
+              columns={columns}
+              onRowClick={handleRowClick}
+              autoHeight
+              sx={{cursor: 'pointer'}}
+              getRowId={r => r.project_id}
+              hideFooter={true}
+              getRowHeight={() => 'auto'}
+              initialState={{
+                sorting: {
+                  sortModel: [sortModel],
+                },
+                pagination: {
+                  paginationModel: {
+                    pageSize: pouchProjectList.length,
+                  },
+                },
+              }}
+              slots={{
+                noRowsOverlay: () => (
+                  <Stack
+                    height="100%"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    No Notebooks have been activated yet.
+                  </Stack>
+                ),
+              }}
+            />
+          </div>
+        </div>
+      </TabPanel>
+      <TabPanel value="2" sx={{px: 0}}>
+        <div style={{display: 'flex', height: '100%'}}>
+          <div style={{flexGrow: 1}}>
+            <DataGrid
+              key={'notebook_list_datagrid'}
+              rows={pouchProjectList.filter(r => !r.is_activated)}
+              loading={loading}
+              columns={columns}
+              autoHeight
+              sx={{cursor: 'pointer'}}
+              getRowId={r => r.project_id}
+              hideFooter={true}
+              getRowHeight={() => 'auto'}
+              initialState={{
+                sorting: {
+                  sortModel: [sortModel],
+                },
+                pagination: {
+                  paginationModel: {
+                    pageSize: pouchProjectList.length,
+                  },
+                },
+              }}
+              slots={{
+                noRowsOverlay: () => (
+                  <Stack
+                    height="100%"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    You don't have any unactivated notebooks.
+                  </Stack>
+                ),
+              }}
+            />
+          </div>
+        </div>
+      </TabPanel>
+    </TabContext>
+  );
+}
