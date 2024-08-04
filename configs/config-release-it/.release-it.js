96c8aa65d5f64e34311bdeb0878c6c296e8b268c
Peter Baker <peter.baker122@csiro.au>
2024-08-05 09:57:00 +1000

    initial update of packages etc
    Signed-off-by: Peter Baker <peter.baker122@csiro.au>

---
+++ configs/config-release-it/.release-it.js
@@ -0,0 +1,39 @@
+const version = '${version}';
+const packageName = process.env.npm_package_name;
+const scope = packageName.split('/')[1];
+
+module.exports = {
+  plugins: {
+    '@release-it/conventional-changelog': {
+      path: '.',
+      infile: 'CHANGELOG.md',
+      preset: 'conventionalcommits',
+      gitRawCommitsOpts: {
+        path: '.',
+      },
+    },
+  },
+  git: {
+    push: true,
+    tagName: `${packageName}-v${version}`,
+    pushRepo: 'git@github.com:FAIMS/FAIMS3.git',
+    commitsPath: '.',
+    commitMessage: `feat(${scope}): released version v${version} [no ci]`,
+    requireCommits: true,
+    requireCommitsFail: false,
+  },
+  npm: {
+    publish: false,
+    versionArgs: ['--workspaces false'],
+  },
+  github: {
+    release: true,
+    releaseName: `${packageName}-v${version}`,
+  },
+  hooks: {
+    'before:git:release': [
+      'mvm-update',
+      'git add --all',
+    ],
+  },
+};
