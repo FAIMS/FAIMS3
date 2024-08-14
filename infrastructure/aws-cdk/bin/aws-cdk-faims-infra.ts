af0f135b1827255f638cb46154ea564885c05e38
Peter Baker <peter.baker122@csiro.au>
2024-08-14 15:17:49 +1000

    Minor changes - adding clean to turbo tasks and removing unused variable

--- infrastructure/aws-cdk/bin/aws-cdk-faims-infra.ts
+++ infrastructure/aws-cdk/bin/aws-cdk-faims-infra.ts
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import {HostedZoneAttributes} from 'aws-cdk-lib/aws-route53';
 import {FaimsInfraStack, loadConfig} from '../lib/faims-infra-stack';
 import * as path from 'path';
 
@@ -17,13 +16,6 @@ const configFilePath = path.resolve(__dirname, '../configs', configFileName);
 const config = loadConfig(configFilePath);
 
 const app = new cdk.App();
-
-// Set up hosted zone attributes
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const zoneAttributes: HostedZoneAttributes = {
-  hostedZoneId: config.hostedZone.id,
-  zoneName: config.hostedZone.name,
-};
 
 // Create the stack
 new FaimsInfraStack(app, config.stackName, {
