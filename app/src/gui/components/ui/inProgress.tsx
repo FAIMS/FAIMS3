455a1e16d2ff185a19f5e3599ac2ba8ad5850c84
root <root@WEDNESDAY-CL.nexus.csiro.au>
2024-08-20 23:57:58 +1000

    minor enhancements to inProgress component
    Signed-off-by: root <root@WEDNESDAY-CL.nexus.csiro.au>

--- app/src/gui/components/ui/inProgress.tsx
+++ app/src/gui/components/ui/inProgress.tsx
@@ -14,24 +14,48 @@
  * limitations under the License.
  *
  * Filename: inProgress.tsx
- * Description:
- *   TODO
+ * Description: A simple component to display a "Feature in progress" message with an icon.
+ * 
  */
 
 import TimelapseIcon from '@mui/icons-material/Timelapse';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import React from 'react';
 
+/**
+ * InProgress Component
+ * 
+ * This component is used to indicate that a feature is currently in progress.
+ * It displays an icon and a text message, styled to be responsive and visually appealing.
+ *
+ * @component
+ * @example
+ * return (
+ *   <InProgress />
+ * )
+ */
 export default function InProgress() {
   return (
-    <div
-      style={{
+    <Box
+      sx={{
         display: 'flex',
         alignItems: 'center',
         flexWrap: 'wrap',
+        p: 2,
+        borderRadius: 2,
+        bgcolor: 'background.paper',
+        boxShadow: 1,
+        maxWidth: '100%',
+        width: 'auto',
+        margin: 'auto',
+        textAlign: 'center',
       }}
     >
-      <TimelapseIcon color={'primary'} />
-      &nbsp;&nbsp;<span>Feature in progress</span>
-    </div>
+      <TimelapseIcon color="primary" sx={{ fontSize: 40, mr: 2 }} />
+      <Typography variant="h6" component="span">
+        Feature in progress
+      </Typography>
+    </Box>
   );
 }
