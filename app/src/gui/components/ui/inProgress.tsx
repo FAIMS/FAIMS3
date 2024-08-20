128a5fcd24b3e9433145d33de897b774b3adf086
root <root@WEDNESDAY-CL.nexus.csiro.au>
2024-08-21 00:07:55 +1000

    lint fixes.
    Signed-off-by: root <root@WEDNESDAY-CL.nexus.csiro.au>

--- app/src/gui/components/ui/inProgress.tsx
+++ app/src/gui/components/ui/inProgress.tsx
@@ -2,20 +2,19 @@
  * Copyright 2021, 2022 Macquarie University
  *
  * Licensed under the Apache License Version 2.0 (the, "License");
- * you may not use, this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing software
- * distributed under the License is distributed on an "AS IS" BASIS
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND either express or implied.
- * See, the License, for the specific language governing permissions and
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  *
  * Filename: inProgress.tsx
  * Description: A simple component to display a "Feature in progress" message with an icon.
- * 
  */
 
 import TimelapseIcon from '@mui/icons-material/Timelapse';
@@ -25,7 +24,6 @@ import React from 'react';
 
 /**
  * InProgress Component
- * 
  * This component is used to indicate that a feature is currently in progress.
  * It displays an icon and a text message, styled to be responsive and visually appealing.
  *
