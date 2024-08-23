174b44b11eb7877bbdc6a6b4b8dcbb2153092297
Steve Cassidy <steve.cassidy@mq.edu.au>
2024-08-23 22:02:10 +1000

    initial implementation of address field
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

---
+++ app/src/gui/fields/Address.tsx
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2021, 2022 Macquarie University
+ *
+ * Licensed under the Apache License Version 2.0 (the, "License");
+ * you may not use, this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing software
+ * distributed under the License is distributed on an "AS IS" BASIS
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND either express or implied.
+ * See, the License, for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Filename: Address.tsx
+ * Description:
+ *   Implements an address field
+ */
+
+import React, {useState} from 'react';
+import {FieldProps} from 'formik';
+import {Stack, TextField} from '@mui/material';
+import _ from 'lodash';
+
+interface Props {
+  helperText?: string;
+  label?: string;
+}
+
+interface AddressType {
+  house_number?: string;
+  road?: string;
+  town?: string;
+  suburb?: string;
+  municipality?: string;
+  state?: string;
+  postcode?: string;
+  country?: string;
+  country_code?: 'au';
+}
+
+export const AddressField = (props: FieldProps & Props) => {
+  const [address, setAddress] = useState<AddressType>(
+    props.field.value.address || {}
+  );
+  const [displayName, setDisplayName] = useState(
+    props.field.value.display_name || ''
+  );
+
+  const setFieldValue = (a: AddressType) => {
+    const dn = `${a.house_number || ''} ${a.road || ''}, ${a.suburb || ''}, ${a.postcode || ''}`;
+    setDisplayName(dn);
+    setAddress(a);
+    props.form.setFieldValue(props.field.name, {
+      display_name: dn,
+      address: a,
+    });
+  };
+
+  const updateProperty = (
+    propName: string
+  ): React.ChangeEventHandler<HTMLInputElement> => {
+    return event => {
+      const newAddress: any = _.cloneDeep(address); // coerce to any to allow property assignment below
+      newAddress[propName] = event.target.value;
+      setFieldValue(newAddress);
+    };
+  };
+
+  return (
+    <div>
+      <Stack spacing={2}>
+        <div>{props.label}</div>
+        <div>{props.helperText}</div>
+
+        <TextField value={displayName} label="Address" fullWidth disabled />
+
+        <TextField
+          label="House Number"
+          value={address.house_number}
+          fullWidth
+          onChange={updateProperty('house_number')}
+        />
+        <TextField
+          label="Street Name"
+          value={address.road}
+          fullWidth
+          onChange={updateProperty('road')}
+        />
+        <TextField
+          label="Suburb"
+          value={address.suburb}
+          fullWidth
+          onChange={updateProperty('suburb')}
+        />
+        <TextField
+          label="Postcode"
+          value={address.postcode}
+          fullWidth
+          onChange={updateProperty('postcode')}
+        />
+
+      </Stack>
+    </div>
+  );
+};
