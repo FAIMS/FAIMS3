7861a2db145e543614d1aa194b8bdc604f61a28e
Ranisa Gupta <ranisa.gupta@csiro.au>
2024-10-08 00:00:47 +1100

    Added a custom styling for BSS Tabs in alignment with BSS Figma theme, different colors and UI effects to be displayed that aligns with Figma Design
    Signed-off-by: Ranisa Gupta <ranisa.gupta@csiro.au>

--- app/src/gui/themes/index.tsx
+++ app/src/gui/themes/index.tsx
@@ -75,8 +75,53 @@ const exportAppBarHeading = () => {
   }
 };
 
+/**
+ * Exports custom styling for the tabs based on the current theme.
+ *
+ * This function provides specific styles for the "bubble" theme, including background color, border,
+ * tab text styling, hover effects, and tab indicator customization. For other themes, it returns
+ * an empty style object.
+ *
+ * @returns {object} The styling object for the tabs:
+ *  - `tabsRoot`: Styles for the root of the `Tabs` component, including background color and border.
+ *  - `tabRoot`: Styles for each individual `Tab` component, including font size, font weight, color, and hover effects.
+ *  - `indicator`: Styles for the tab indicator, including color and thickness.
+ */
+const exportTabStyling = () => {
+  switch (import.meta.env.VITE_THEME) {
+    case 'bubble':
+      return {
+        tabsRoot: {
+          backgroundColor: '#f5f5f5',
+          borderBottom: '1px solid grey',
+        },
+        tabRoot: {
+          fontWeight: 600,
+          fontSize: '1rem',
+          textTransform: 'none',
+          minWidth: 100,
+          color: '#000000',
+          '&.Mui-selected': {
+            color: 'white',
+            backgroundColor: '#050606e8',
+          },
+          '&:hover': {
+            backgroundColor: 'black',
+          },
+        },
+        indicator: {
+          backgroundColor: 'white',
+          height: 4,
+        },
+      };
+    default:
+      return {};
+  }
+};
+
 export const theme = exportTheme();
 export const appBarStyling = exportAppBarStyling();
 export const projectListLayout = exportProjectListLayout();
 export const projectListVerbose = exportProjectListVerbose();
 export const appBarHeading = exportAppBarHeading();
+export const tabStyling = exportTabStyling();
