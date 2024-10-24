9c157994e2c836b0d4114ec9e1f4f14f019c6b84
Steve Cassidy <steve.cassidy@mq.edu.au>
2024-10-24 17:05:54 +1100

    in fact can't have any non-config files in that dir, create it from the shell script
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- app/bin/prebuildConfig.sh
+++ app/bin/prebuildConfig.sh
@@ -23,10 +23,17 @@ sed -i -e "s/${APP_NAME_PLACEHOLDER}/${VITE_APP_NAME}/g" android/app/src/main/An
 
 # android/app/src/main/res/values/strings.xml
 
-sed -e "s/${APP_NAME_PLACEHOLDER}/${VITE_APP_NAME}/g"  ./android/app/src/main/res/values/strings.xml.dist |\
-  sed -e "s/${APP_ID_PLACEHOLDER}/${VITE_APP_ID}/g" > ./android/app/src/main/res/values/strings.xml
+# sed -e "s/${APP_NAME_PLACEHOLDER}/${VITE_APP_NAME}/g"  ./android/app/src/main/res/values/strings.xml.dist |\
+#   sed -e "s/${APP_ID_PLACEHOLDER}/${VITE_APP_ID}/g" > ./android/app/src/main/res/values/strings.xml
 
-
+cat << EOT > ./android/app/src/main/res/values/strings.xml
+<?xml version='1.0' encoding='utf-8'?>
+<resources>
+    <string name="app_name">${VITE_APP_NAME}</string>
+    <string name="title_activity_main">${VITE_APP_NAME}</string>
+    <string name="custom_url_scheme">${VITE_APP_ID}</string>
+</resources>
+EOT
 
 ## Now run the asset generation script
 
