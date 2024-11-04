38ae0428e484d1b2e5bf01f6993cd2a2753ea245
Luke McFarlane <luke.mcfarlane@rocketlab.com.au>
2024-11-04 14:58:44 +1100

    fixed icon centering
    Signed-off-by: Luke McFarlane <luke.mcfarlane@rocketlab.com.au>

--- app/src/gui/components/app-bar/app-bar-heading.tsx
+++ app/src/gui/components/app-bar/app-bar-heading.tsx
@@ -13,26 +13,17 @@ interface AppBarHeadingProps {
  * @returns {JSX.Element} - The rendered AppBarHeading component.
  */
 export const AppBarHeading = ({link}: AppBarHeadingProps) => (
-  <>
-    <NavLink style={{flexGrow: 0}} to={link}>
-      <img
-        src="/assets/icons/icon-48.webp"
-        style={{maxWidth: '140px', flex: 1}}
-      />
-    </NavLink>
-    <NavLink
-      to={link}
-      style={{
-        flexGrow: 1,
-        fontSize: 32,
-        fontWeight: 600,
-        textAlign: 'left',
-        textDecoration: 'none',
-        color: 'inherit',
-        fontVariant: 'small-caps',
-      }}
-    >
-      {APP_NAME}
-    </NavLink>
-  </>
+  <NavLink
+    to={link}
+    style={{
+      display: 'flex',
+      alignItems: 'center',
+      flex: 1,
+      textDecoration: 'none',
+      color: 'black',
+    }}
+  >
+    <img src="/assets/icons/icon-48.webp" />
+    <h1>{APP_NAME}</h1>
+  </NavLink>
 );
