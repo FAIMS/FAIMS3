5579308579dba098e9669487dc800279811e80c7
Steve Cassidy <steve.cassidy@mq.edu.au>
2024-11-08 16:26:01 +1100

    remove unused useEffect and remove repetition
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- app/src/gui/fields/TakePoint.tsx
+++ app/src/gui/fields/TakePoint.tsx
@@ -128,25 +128,31 @@ export const TakePoint = (
       </Button>
       {positionText}
       {error_text}
-      {noPermission && Capacitor.getPlatform() === 'web' && (
+      {noPermission && (
         <Alert severity="error" sx={{width: '100%'}}>
-          Please enable location permissions for this page. In your browser,
-          look to the left of the web address bar for a button that gives access
-          to browser settings for this page.
-        </Alert>
-      )}
-      {noPermission && Capacitor.getPlatform() === 'android' && (
-        <Alert severity="error" sx={{width: '100%'}}>
-          Please enable location permissions for {APP_NAME}. Go to your device
-          Settings &gt; Apps &gt; {APP_NAME} &gt; Permissions &gt; Location and
-          select "Allow all the time" or "Allow only while using the app".
-        </Alert>
-      )}
-      {noPermission && Capacitor.getPlatform() === 'ios' && (
-        <Alert severity="error" sx={{width: '100%'}}>
-          Please enable location permissions for {APP_NAME}. Go to your device
-          Settings &gt; Privacy & Security &gt; Location Services &gt;
-          {APP_NAME} and select "While Using the App".
+          {Capacitor.getPlatform() === 'web' && (
+            <>
+              Please enable location permissions for this page. In your browser,
+              look to the left of the web address bar for a button that gives
+              access to browser settings for this page.
+            </>
+          )}
+          {Capacitor.getPlatform() === 'android' && (
+            <>
+              Please enable location permissions for {APP_NAME}. Go to your
+              device Settings &gt; Apps &gt; {APP_NAME} &gt; Permissions &gt;
+              Location and select "Allow all the time" or "Allow only while
+              using the app".
+            </>
+          )}
+          {Capacitor.getPlatform() === 'ios' && (
+            <>
+              Please enable location permissions for {APP_NAME}. Go to your
+              device Settings &gt; Privacy & Security &gt; Location Services
+              &gt;
+              {APP_NAME} and select "While Using the App".
+            </>
+          )}
         </Alert>
       )}
     </div>
--- app/src/gui/fields/maps/MapFormField.tsx
+++ app/src/gui/fields/maps/MapFormField.tsx
@@ -56,17 +56,6 @@ export function MapFormField({
   const [drawnFeatures, setDrawnFeatures] =
     useState<GeoJSONFeatureCollection>(initialFeatures);
 
-  useEffect(() => {
-    const perms = async () => {
-      const permission = await Geolocation.checkPermissions();
-      console.log('permission', permission);
-      if (permission.location === 'granted') {
-        await Geolocation.requestPermissions({permissions: ['location']});
-      }
-    };
-    perms();
-  }, []);
-
   // default props.center if not defined
   if (!props.center) {
     props.center = [0, 0];
@@ -137,25 +126,31 @@ export function MapFormField({
           projection={props.projection}
           setNoPermission={setNoPermission}
         />
-        {noPermission && Capacitor.getPlatform() === 'web' && (
+        {noPermission && (
           <Alert severity="error" sx={{width: '100%'}}>
-            Please enable location permissions for this page. In your browser,
-            look to the left of the web address bar for a button that gives
-            access to browser settings for this page.
-          </Alert>
-        )}
-        {noPermission && Capacitor.getPlatform() === 'android' && (
-          <Alert severity="error" sx={{width: '100%'}}>
-            Please enable location permissions for {APP_NAME}. Go to your device
-            Settings &gt; Apps &gt; {APP_NAME} &gt; Permissions &gt; Location
-            and select "Allow all the time" or "Allow only while using the app".
-          </Alert>
-        )}
-        {noPermission && Capacitor.getPlatform() === 'ios' && (
-          <Alert severity="error" sx={{width: '100%'}}>
-            Please enable location permissions for {APP_NAME}. Go to your device
-            Settings &gt; Privacy & Security &gt; Location Services &gt;
-            {APP_NAME} and select "While Using the App".
+            {Capacitor.getPlatform() === 'web' && (
+              <>
+                Please enable location permissions for this page. In your
+                browser, look to the left of the web address bar for a button
+                that gives access to browser settings for this page.
+              </>
+            )}
+            {Capacitor.getPlatform() === 'android' && (
+              <>
+                Please enable location permissions for {APP_NAME}. Go to your
+                device Settings &gt; Apps &gt; {APP_NAME} &gt; Permissions &gt;
+                Location and select "Allow all the time" or "Allow only while
+                using the app".
+              </>
+            )}
+            {Capacitor.getPlatform() === 'ios' && (
+              <>
+                Please enable location permissions for {APP_NAME}. Go to your
+                device Settings &gt; Privacy & Security &gt; Location Services
+                &gt;
+                {APP_NAME} and select "While Using the App".
+              </>
+            )}
           </Alert>
         )}
         <p>{valueText}</p>
