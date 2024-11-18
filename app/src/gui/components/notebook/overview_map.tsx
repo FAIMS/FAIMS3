636ac882439d165aac09561acaeb518a5b7c12b4
Steve Cassidy <steve.cassidy@mq.edu.au>
2024-11-18 13:25:53 +1100

    Initial working version of overview map
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- app/src/gui/components/notebook/overview_map.tsx
+++ app/src/gui/components/notebook/overview_map.tsx
@@ -1,7 +1,6 @@
 import Map from 'ol/Map';
 import {
   getRecordsWithRegex,
-  hydrateRecord,
   ProjectID,
   ProjectUIModel,
 } from '@faims3/data-model';
@@ -27,22 +26,54 @@ interface OverviewMapProps {
 
 export const OverviewMap = (props: OverviewMapProps) => {
   const [map, setMap] = useState<Map | undefined>();
-  const [featuresLayer, setFeaturesLayer] =
-    useState<VectorLayer<Feature<Geometry>>>();
   const defaultMapProjection = 'EPSG:3857';
   const geoJson = new GeoJSON();
+  const [features, setFeatures] = useState<{type: string; features: any[]}>({
+    type: 'FeatureCollection',
+    features: [],
+  });
 
-  console.log('is it a function:', hydrateRecord);
+  const getGISFields = (uiSpec: ProjectUIModel) => {
+    const fields = Object.getOwnPropertyNames(uiSpec.fields);
+    return fields.filter(
+      (field: string) =>
+        uiSpec.fields[field]['component-name'] === 'MapFormField' ||
+        uiSpec.fields[field]['component-name'] === 'TakePoint'
+    );
+  };
+
   useEffect(() => {
-    console.log(props.project_id, Object.getOwnPropertyNames(props.uiSpec.views)[0]);
     const init = async () => {
-      console.log('doing init');
-      const records = await getRecordsWithRegex(props.project_id, '.*', true);
-      for (const record in records) {
-        const full_record = await hydrateRecord(props.project_id, record);
-        console.log(full_record);
+      const gisFields = getGISFields(props.uiSpec);
+      console.log('gisFields', gisFields);
+      if (gisFields.length > 0) {
+        const records = await getRecordsWithRegex(props.project_id, '.*', true);
+        const f: any[] = [];
+        records.forEach(record => {
+          if (record.data) {
+            gisFields.forEach((field: string) => {
+              // two options here, if it's a TakePoint field we'll have a single feature
+              // if it's a MapFormField we'll have an object with multiple features
+              if (record.data?.[field] && record.data[field].type) {
+                if (record.data[field].type === 'FeatureCollection') {
+                  record.data[field].features.forEach((feature: any) => {
+                    f.push(feature);
+                  });
+                } else {
+                  f.push(record.data[field]);
+                }
+              }
+            });
+          }
+        });
+        console.log('collected features', f);
+        setFeatures({
+          type: 'FeatureCollection',
+          features: f,
+        });
       }
     };
+
     init().catch(console.error);
   }, []);
 
@@ -53,88 +84,78 @@ export const OverviewMap = (props: OverviewMapProps) => {
 
   const map_center = [30, -10];
 
-  const createMap = useCallback(
-    async (element: HTMLElement, props: OverviewMapProps): Promise<Map> => {
-      const center = transform(map_center, 'EPSG:4326', defaultMapProjection);
+  const createMap = useCallback(async (element: HTMLElement): Promise<Map> => {
+    const center = transform(map_center, 'EPSG:4326', defaultMapProjection);
 
-      const tileLayer = new TileLayer({source: new OSM()});
-      const view = new View({
-        projection: defaultMapProjection,
-        center: center,
-        zoom: 12,
-      });
+    const tileLayer = new TileLayer({source: new OSM()});
+    const view = new View({
+      projection: defaultMapProjection,
+      center: center,
+      zoom: 12,
+    });
 
-      const theMap = new Map({
-        target: element,
-        layers: [tileLayer],
-        view: view,
-        controls: [new Zoom()],
-      });
+    const theMap = new Map({
+      target: element,
+      layers: [tileLayer],
+      view: view,
+      controls: [new Zoom()],
+    });
 
-      theMap.addControl(createCenterControl(theMap.getView(), center));
+    theMap.addControl(createCenterControl(theMap.getView(), center));
 
-      theMap.getView().setCenter(center);
+    theMap.getView().setCenter(center);
 
-      return theMap;
-    },
-    []
-  );
+    return theMap;
+  }, []);
 
-  const addDrawInteraction = useCallback(
-    (map: Map, props: OverviewMapProps) => {
-      const source = new VectorSource();
+  const addFeaturesToMap = (map: Map) => {
+    const source = new VectorSource();
 
-      const layer = new VectorLayer({
-        source: source,
-        style: new Style({
-          stroke: new Stroke({
-            color: '#33ff33',
-            width: 4,
-          }),
-          image: new CircleStyle({
-            radius: 7,
-            fill: new Fill({color: '#33ff33'}),
-          }),
+    const layer = new VectorLayer({
+      source: source,
+      style: new Style({
+        stroke: new Stroke({
+          color: '#33ff33',
+          width: 4,
         }),
+        image: new CircleStyle({
+          radius: 7,
+          fill: new Fill({color: '#33ff33'}),
+        }),
+      }),
+    });
+
+    if (features && features.features.length > 0) {
+      const parsedFeatures = geoJson.readFeatures(features, {
+        dataProjection: 'EPSG:4326',
+        featureProjection: map.getView().getProjection(),
       });
+      source.addFeatures(parsedFeatures);
 
-      //   // add features to map if we're passed any in
-      //   if (props.features && props.features.type) {
-      //     const parsedFeatures = geoJson.readFeatures(props.features, {
-      //       dataProjection: 'EPSG:4326',
-      //       featureProjection: map.getView().getProjection(),
-      //     });
-      //     source.addFeatures(parsedFeatures);
+      // set the view so that we can see the features
+      // but don't zoom too much
+      const extent = source.getExtent();
+      // don't fit if the extent is infinite because it crashes
+      if (!extent.includes(Infinity)) {
+        map.getView().fit(extent, {padding: [100, 100, 100, 100], maxZoom: 12});
+      }
+    }
 
-      //     // set the view so that we can see the features
-      //     // but don't zoom too much
-      //     const extent = source.getExtent();
-      //     // don't fit if the extent is infinite because it crashes
-      //     if (!extent.includes(Infinity)) {
-      //       map
-      //         .getView()
-      //         .fit(extent, {padding: [20, 20, 20, 20], maxZoom: props.zoom});
-      //     }
-      //   }
-
-      // setDrawInteraction(draw)
-
-      map.addLayer(layer);
-      setFeaturesLayer(layer);
-    },
-    [setFeaturesLayer]
-  );
+    map.addLayer(layer);
+  };
 
   const refCallback = (element: HTMLElement | null) => {
+    console.log('refCallback', element);
     if (element) {
       if (!map) {
         // create map
         createMap(element, props).then((theMap: Map) => {
-          addDrawInteraction(theMap, props);
+          addFeaturesToMap(theMap);
           setMap(theMap);
         });
       } else {
         map.setTarget(element);
+        addFeaturesToMap(map);
       }
     }
   };
