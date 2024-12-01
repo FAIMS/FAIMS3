6f872a14903ab91cd96c8dcf385c6f8e529b1224
Steve Cassidy <steve.cassidy@mq.edu.au>
2024-12-02 10:30:24 +1100

    move IOS version/id setting into main config script
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- app/bin/prebuildConfig.sh
+++ app/bin/prebuildConfig.sh
@@ -47,3 +47,20 @@ if test -f icons/icon-48.webp; then
   mkdir -p ./public/assets
   mv icons ./public/assets/icons
 fi
+
+
+## IOS specific configuration
+
+# Setting the IOS build version
+# https://pgu.dev/2020/12/16/ios-build-versioning.html
+
+#version=$(grep '"version":' $PROJECT_DIR/package.json | cut -d: -f 2 | sed -e 's/[", ]//g')
+# since we previously published 1.0.0 on app store we need a higher version
+# number, set that here until we get the main version above this
+version=1.0.4
+buildNumber=$(date -u "+%Y%m%d%H%M")
+
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" ./ios/App/App/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $version" ./ios/App/App/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleURLTypes:0:CFBundleURLSchemes:0 $VITE_APP_ID" ./ios/App/App/Info.plist
+/usr/libexec/PlistBuddy -c "Set :CFBundleURLTypes:0:CFBundleURLName $VITE_APP_ID" ./ios/App/App/Info.plist
