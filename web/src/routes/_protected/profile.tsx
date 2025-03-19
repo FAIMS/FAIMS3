3eea4d57975e3930309a65fb0ccf4a715821f9cf
Luke McFarlane <luke.mcfarlane@rocketlab.com.au>
2025-03-19 14:29:50 +1100

    Added bearer token copy, cleaned up profile page, fixed bug with breadcrumbs
    Signed-off-by: Luke McFarlane <luke.mcfarlane@rocketlab.com.au>

--- web/src/routes/_protected/profile.tsx
+++ web/src/routes/_protected/profile.tsx
@@ -1,25 +1,53 @@
-import {useAuth} from '@/context/auth-provider';
+import {useAuth, User} from '@/context/auth-provider';
 import {Card} from '@/components/ui/card';
 import {createFileRoute} from '@tanstack/react-router';
+import {List, ListDescription, ListItem, ListLabel} from '@/components/ui/list';
+import {Button} from '@/components/ui/button';
+import {toast} from 'sonner';
 
 export const Route = createFileRoute('/_protected/profile')({
   component: RouteComponent,
 });
 
-/**
- * RouteComponent component renders the profile page.
- * It displays a card with the user's profile information.
- *
- * @returns {JSX.Element} The rendered RouteComponent component.
- */
+const userFields: {field: keyof User['user']; label: string}[] = [
+  {field: 'id', label: 'Email'},
+  {field: 'name', label: 'Name'},
+];
+
 function RouteComponent() {
-  const auth = useAuth();
+  const {user} = useAuth();
 
   return (
-    <Card>
-      <pre className="text-xs text-wrap break-words">
-        {JSON.stringify(auth.user, null, 2)}
-      </pre>
-    </Card>
+    <div className="flex lg:flex-row flex-col gap-4">
+      <Card className="flex-1">
+        <List>
+          {userFields.map(({field, label}) => (
+            <ListItem key={field}>
+              <ListLabel>{label}</ListLabel>
+              <ListDescription>{user?.user[field]}</ListDescription>
+            </ListItem>
+          ))}
+        </List>
+      </Card>
+      <Card className="flex-1">
+        <ListItem className="flex flex-col gap-2">
+          <ListLabel>Bearer Token</ListLabel>
+          <ListDescription>
+            Click below to copy the token that can be used to authenticate in
+            scripts that use the API.
+          </ListDescription>
+          <Button
+            variant="outline"
+            onClick={() => {
+              navigator.clipboard.writeText(user?.token || '');
+
+              toast('Bearer token copied to clipboard successfully! 🎉');
+            }}
+          >
+            Copy Bearer Token to Clipboard
+          </Button>
+        </ListItem>
+      </Card>
+    </div>
   );
 }
