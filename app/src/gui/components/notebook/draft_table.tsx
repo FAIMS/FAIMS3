9754356351aadf1a0b21a9e22fe3ca2bf716b6e9
Ranisa Gupta <ranisa.gupta@csiro.au>
2025-03-21 15:35:25 +1100

    drafts table consistency
    Signed-off-by: Ranisa Gupta <ranisa.gupta@csiro.au>

--- app/src/gui/components/notebook/draft_table.tsx
+++ app/src/gui/components/notebook/draft_table.tsx
@@ -48,8 +48,8 @@ export function DraftsTable(props: DraftsRecordProps) {
 
   const mobileView: boolean = not_xs;
   const defaultMaxRowsMobile = 10;
+  const isMobile = !useMediaQuery(theme.breakpoints.up('sm'));
 
-  // The entire row is clickable to the record
   const handleRowClick: GridEventListener<'rowClick'> = params => {
     history(
       ROUTES.getDraftRoute(
@@ -63,7 +63,6 @@ export function DraftsTable(props: DraftsRecordProps) {
   };
 
   function getRowType(params: GridCellParams) {
-    // The type (or Kind) is prettified and should be filterable as such.
     return props.viewsets !== null &&
       props.viewsets !== undefined &&
       params.row.type !== null &&
@@ -76,114 +75,96 @@ export function DraftsTable(props: DraftsRecordProps) {
   const columns: any[] = !mobileView
     ? [
         {
-          field: 'draft_icon',
-          headerName: '',
-          type: 'string',
-          width: 40,
-          renderCell: () => <ArticleOutlinedIcon sx={{my: 2}} />,
-          hide: false,
-          sortable: false,
-          filterable: false,
-          disableColumnMenu: true,
-        },
-        {
-          field: 'type',
-          headerName: 'Kind',
-          type: 'string',
-          width: 200,
-          valueGetter: getRowType,
-        },
-        {
-          field: '_id',
-          headerName: 'Draft ID',
-          description: 'Draft ID',
-          type: 'string',
-          flex: 0.5,
-          minWidth: 400,
-          renderCell: (params: GridCellParams) => params.value,
-        },
-        {
           field: 'hrid',
-          headerName: 'HRID/UUID',
+          headerName: 'Field ID',
           description: 'Human Readable Record ID',
           type: 'string',
-          width: 300,
+          flex: 1,
           renderCell: (params: GridCellParams) => (
-            <Link underline={'none'} sx={{fontWeight: 'bold'}}>
-              {params.row.hrid}
-            </Link>
+            <Typography fontWeight="bold">{params.row.hrid}</Typography>
           ),
         },
-        {field: 'updated', headerName: 'Updated', type: 'dateTime', width: 200},
-        {field: 'created', headerName: 'Created', type: 'dateTime', width: 200},
+        // {
+        //   field: 'created',
+        //   headerName: 'Created',
+        //   type: 'dateTime',
+        //   width: 200,
+        //   renderCell: (params: GridCellParams) => (
+        //     <Typography>
+        //       {params.row.created
+        //         ? new Date(params.row.created).toLocaleString()
+        //         : '-'}
+        //     </Typography>
+        //   ),
+        // },
+        // {
+        //   field: 'created_by',
+        //   headerName: 'Created By',
+        //   type: 'string',
+        //   width: 150,
+        //   renderCell: (params: GridCellParams) => (
+        //     <Typography>{params.row.created_by || '-'}</Typography>
+        //   ),
+        // },
+        // {
+        //   field: 'updated',
+        //   headerName: 'Last Updated',
+        //   type: 'dateTime',
+        //   width: 200,
+        //   renderCell: (params: GridCellParams) => (
+        //     <Typography>
+        //       {params.row.updated
+        //         ? new Date(params.row.updated).toLocaleString()
+        //         : '-'}
+        //     </Typography>
+        //   ),
+        // },
+        // {
+        //   field: 'updated_by',
+        //   headerName: 'Last Updated By',
+        //   type: 'string',
+        //   width: 150,
+        //   renderCell: (params: GridCellParams) => (
+        //     <Typography>{params.row.updated_by || '-'}</Typography>
+        //   ),
+        // },
         {
           field: 'delete',
           headerName: 'Actions',
           type: 'actions',
-          renderCell: (params: GridCellParams) => {
-            return (
-              <RecordDelete
-                project_id={project_id}
-                serverId={serverId}
-                record_id={params.row.record_id}
-                revision_id={params.row.revision_id}
-                draft_id={params.row._id}
-                show_label={false}
-                handleRefresh={props.handleRefresh}
-              />
-            );
-          },
+          renderCell: (params: GridCellParams) => (
+            <RecordDelete
+              project_id={project_id}
+              serverId={serverId}
+              record_id={params.row.record_id}
+              revision_id={params.row.revision_id}
+              draft_id={params.row._id}
+              show_label={false}
+              handleRefresh={props.handleRefresh}
+            />
+          ),
         },
       ]
     : [
         {
-          field: '_id',
-          headerName: 'Draft ID',
-          description: 'Draft ID',
+          field: 'summary',
+          headerName: 'Details',
           type: 'string',
           flex: 1,
           renderCell: (params: GridCellParams) => (
             <Box sx={{width: '100%', my: 1}}>
-              <Grid
-                container
-                direction="row"
-                justifyContent="flex-start"
-                alignItems="center"
-                spacing={0}
-              >
-                <Grid item>
-                  <ArticleOutlinedIcon
-                    fontSize={'small'}
-                    sx={{
-                      verticalAlign: 'middle',
-                      marginRight: '4px',
-                      color: theme.palette.secondary.main,
-                    }}
-                  />
-                </Grid>
-                <Grid item>
-                  <Typography>
-                    Kind:{'  '}
-                    {getRowType(params)}
-                  </Typography>
-                </Grid>
-              </Grid>
-
-              <Typography color="textSecondary">
-                Draft ID: {params.row.value}
+              <Typography fontWeight="bold">
+                Field ID: {params.row.hrid}
+              </Typography>
+              {/* <Typography color="textSecondary">
+                Created: {params.row.created || '-'}
               </Typography>
               <Typography color="textSecondary">
-                HRID/UUID: {params.row.hrid}
+                Created By: {params.row.created_by || '-'}
               </Typography>
-
-              <Typography
-                color="textSecondary"
-                variant="subtitle2"
-                gutterBottom
-                component="div"
-              >
-                Updated: {(params.row.updated || '').toString()}
-              </Typography>
+              <Typography color="textSecondary">
+                Last Updated: {params.row.updated || '-'}
+              </Typography> */}
             </Box>
           ),
         },
@@ -191,19 +172,17 @@ export function DraftsTable(props: DraftsRecordProps) {
           field: 'delete',
           headerName: 'Actions',
           type: 'actions',
-          renderCell: (params: GridCellParams) => {
-            return (
-              <RecordDelete
-                project_id={project_id}
-                serverId={serverId}
-                record_id={params.row.record_id}
-                revision_id={params.row.revision_id}
-                draft_id={params.row._id}
-                show_label={false}
-                handleRefresh={props.handleRefresh}
-              />
-            );
-          },
+          renderCell: (params: GridCellParams) => (
+            <RecordDelete
+              project_id={project_id}
+              serverId={serverId}
+              record_id={params.row.record_id}
+              revision_id={params.row.revision_id}
+              draft_id={params.row._id}
+              show_label={false}
+              handleRefresh={props.handleRefresh}
+            />
+          ),
         },
       ];
 
@@ -219,20 +198,20 @@ export function DraftsTable(props: DraftsRecordProps) {
           autoHeight
           sx={{
             cursor: 'pointer',
-            padding: '8px',
-            backgroundColor: theme.palette.background.tabsBackground,
+            backgroundColor: theme.palette.background.default,
             borderRadius: '4px',
             boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.2)',
             mb: 2,
             '& .MuiDataGrid-columnHeaders': {
-              backgroundColor: theme.palette.background.default,
-              borderBottom: '1px solid #ccc',
+              backgroundColor: theme.palette.grey[100],
+              borderBottom: '2px solid #ccc',
             },
             '& .MuiDataGrid-columnSeparator': {
               visibility: 'visible',
               color: '#ccc',
             },
             '& .MuiDataGrid-cell': {
+              padding: '16px 24px',
               borderBottom: '1px solid #eee',
             },
           }}
@@ -246,19 +225,15 @@ export function DraftsTable(props: DraftsRecordProps) {
             filterPanel: {sx: {maxWidth: '96vw'}},
           }}
           initialState={{
-            sorting: {
-              sortModel: [{field: 'updated', sort: 'desc'}],
-            },
+            sorting: {sortModel: [{field: 'created', sort: 'desc'}]},
             pagination: {
               paginationModel: {
                 pageSize:
                   maxRows !== null
-                    ? not_xs
-                      ? maxRows
-                      : defaultMaxRowsMobile
-                    : not_xs
-                      ? 25
-                      : defaultMaxRowsMobile,
+                    ? isMobile
+                      ? defaultMaxRowsMobile
+                      : maxRows
+                    : 25,
               },
             },
           }}
