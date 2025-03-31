65e8a0a7c53d93db4479d9d2c59d4eeb07194dfd
Steve Cassidy <steve.cassidy@mq.edu.au>
2025-04-01 10:45:20 +1100

    fix duplicate property
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- app/src/gui/pages/record-create.tsx
+++ app/src/gui/pages/record-create.tsx
@@ -155,7 +155,6 @@ function DraftRecordEdit(props: DraftRecordEditProps) {
   const {type_name, project, draft_id, record_id, serverId} = props;
   const project_id = project.projectId;
   const navigate = useNavigate();
-  const location = useLocation();
   const [isDraftSaving, setIsDraftSaving] = useState(false);
   const [draftLastSaved, setDraftLastSaved] = useState<Date | null>(null);
   const [draftError, setDraftError] = useState<string | null>(null);
@@ -253,7 +252,6 @@ function DraftRecordEdit(props: DraftRecordEditProps) {
               navigate={navigate}
               location={props.location}
               setProgress={setProgress}
-              location={location}
               disabled={false}
             />
           </Box>
