8df626839e890dcd56ae4c1a8a24e319f73e79e5
Luke McFarlane <luke.mcfarlane@rocketlab.com.au>
2025-04-07 16:58:18 +1000

    Removed ExpressUser interface
    Signed-off-by: Luke McFarlane <luke.mcfarlane@rocketlab.com.au>

--- library/data-model/src/types.ts
+++ library/data-model/src/types.ts
@@ -818,21 +818,6 @@ export const TemplateDocumentSchema = z.intersection(
 );
 export type TemplateDocument = z.infer<typeof TemplateDocumentSchema>;
 
-export interface ExpressUser {
-  user_id: string;
-  _id?: string;
-  _rev?: string;
-  name: string;
-  emails: string[];
-  roles: string[];
-  project_roles: {[NonUniqueProjectID: string]: string[]};
-  other_roles: string[];
-  profiles: {
-    [ServiceID: string]: unknown;
-  };
-  owned: NonUniqueProjectID[];
-}
-
 export interface RoleInvite {
   _id: string;
   _rev?: string;
