68de9c59450683711abe04c92a7be90e3125d352
Steve Cassidy <steve.cassidy@mq.edu.au>
2025-04-10 14:51:32 +1000

    Add plain project file back in
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- app/bin/prebuildConfig.sh
+++ app/bin/prebuildConfig.sh
@@ -69,6 +69,6 @@ if test -f /usr/libexec/PlistBuddy; then
 fi
 
 # update project file for local build
-sed -e "s/${APP_NAME_PLACEHOLDER}/${VITE_APP_NAME}/g" ./ios/App/App.xcodeproj/project.pbxproj.dist |\
-  sed -e "s/APP_STORE_CONNECT_TEAM_ID/${APP_STORE_CONNECT_TEAM_ID}/g" > ./ios/App/App.xcodeproj/project.pbxproj
+#sed -e "s/${APP_NAME_PLACEHOLDER}/${VITE_APP_NAME}/g" ./ios/App/App.xcodeproj/project.pbxproj.dist |\
+#  sed -e "s/APP_STORE_CONNECT_TEAM_ID/${APP_STORE_CONNECT_TEAM_ID}/g" > ./ios/App/App.xcodeproj/project.pbxproj
 
