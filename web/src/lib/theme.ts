922df1744a97224a69e2abbff8db8434116bcaff
Steve Cassidy <steve.cassidy@mq.edu.au>
2025-06-02 18:31:51 +1000

    remove extra theme
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- web/src/lib/theme.ts
+++ web/src/lib/theme.ts
@@ -4,8 +4,6 @@ export const getThemeClass = () => {
   switch (THEME) {
     case 'bssTheme':
       return 'theme-bss';
-    case 'red':
-      return 'theme-red';
     case 'default':
     default:
       return 'theme-default';
