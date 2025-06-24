760e97d5be7e8c68f27ff3d616a0187bd85c70b5
Steve Cassidy <steve.cassidy@mq.edu.au>
2025-06-24 13:38:23 +1000

    options for command line script
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- api/src/scripts/restoreBackup.ts
+++ api/src/scripts/restoreBackup.ts
@@ -3,13 +3,20 @@
 import {restoreFromBackup} from '../couchdb/backupRestore';
 
 const main = async () => {
-  if (process.argv.length === 4) {
+  if (process.argv.length >= 4) {
     const filename = process.argv[2];
     const pattern = process.argv[3];
-    console.log(`Restoring from ${filename} with pattern ${pattern}`);
-    restoreFromBackup({filename, pattern});
+    const force = process.argv[4] === 'true';
+    console.log(
+      `Restoring from ${filename} with pattern ${pattern}, force=${force}`
+    );
+    restoreFromBackup({filename, pattern, force});
   } else {
-    console.log('Usage: node restoreBackup.ts <filename> <pattern>');
+    console.log('Usage: node restoreBackup.ts <filename> <pattern> ?<force>');
+    console.log(' * pattern is a regex pattern to filter databases to restore');
+    console.log(
+      ' * last argument is "true" to force overwrite of existing documents, default is false'
+    );
     process.exit(1);
   }
 };
