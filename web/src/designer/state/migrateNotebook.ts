53c01aaf30c7aa049b5bfabece5ab8a57ca9478b
Steve Cassidy <steve.cassidy@mq.edu.au>
2025-07-09 16:53:06 +1000

    remove unused import
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- web/src/designer/state/migrateNotebook.ts
+++ web/src/designer/state/migrateNotebook.ts
@@ -15,7 +15,6 @@
 import {Ajv} from 'ajv';
 import {schema} from '../notebook-schema';
 import {FieldType, Notebook} from './initial';
-import {v4 as uuidv4} from 'uuid';
 
 /**
  * Migrate a notebook to the most recent notebook format
