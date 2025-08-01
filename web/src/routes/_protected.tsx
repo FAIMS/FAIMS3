5b229ab0895f3c721590dbd229524e2c3d752acf
Steve Cassidy <steve.cassidy@mq.edu.au>
2025-08-01 17:19:28 +1000

    Remove the toggle sidebar button - collapsed sidebar breaks because of the Logo, not really useful for this UI
    Signed-off-by: Steve Cassidy <steve.cassidy@mq.edu.au>

--- web/src/routes/_protected.tsx
+++ web/src/routes/_protected.tsx
@@ -144,8 +144,6 @@ function RouteComponent() {
           <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
             <div className="flex justify-between w-full px-4">
               <div className="flex items-center gap-2">
-                <SidebarTrigger className="-ml-1" />
-                <Separator orientation="vertical" className="mr-2 h-4" />
                 <Breadcrumbs />
               </div>
               <div className="flex">
