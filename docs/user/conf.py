61a6fea4e626c2775b8f5db680f53608c293bd28
Steve Cassidy <steve@fieldnote.au>
2025-08-14 16:00:34 +1000

    get documentation config from environment
    Signed-off-by: Steve Cassidy <steve@fieldnote.au>

--- docs/user/conf.py
+++ docs/user/conf.py
@@ -4,13 +4,14 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import sys
+import os
 from pathlib import Path
 sys.path.append(str(Path('_ext').resolve()))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "Fieldmark"
+project = os.getenv("VITE_APP_NAME", "FAIMS")
 copyright = "2023, Electronic Field Notebooks Pty Ltd"
 author = "Electronic Field Notebooks Pty Ltd"
 release = "1.2.2"
@@ -36,12 +37,17 @@ myst_enable_extensions = [
     "substitution",
 ]
 
+NOTEBOOK_NAME = os.getenv("VITE_NOTEBOOK_NAME", "notebook")
+DASHBOARD_NAME = os.getenv("VITE_WEBSITE_TITLE", "dashboard")
+
 myst_substitutions = {
   "FAIMS": project, # the product name
   "theme": "default", # the theme name, used to select screenshots
-  "notebook": "notebook", # the name of a project (notebook or survey)
-  "Notebook": "Notebook", # variations of the notebook name
-  "notebooks": "notebooks",
-  "Notebooks": "Notebooks",
-  "dashboard": "dashboard", # the name of the web management app 
+  "notebook": NOTEBOOK_NAME, # the name of a project (notebook or survey)
+  "Notebook": NOTEBOOK_NAME.title(), # variations of the notebook name
+  "notebooks": NOTEBOOK_NAME + 's',
+  "Notebooks": NOTEBOOK_NAME.title() + 's',
+  "dashboard": DASHBOARD_NAME, # the name of the web management app
 }
+
+print(os.environ)
