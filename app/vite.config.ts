635f20cdecf6ae6709b9d87c71498b6e8c0b5b45
Steve Cassidy <steve@fieldnote.au>
2025-08-22 11:35:54 +1000

    Use Vite to get package version into the app, remove old scripts we didn't use
    Signed-off-by: Steve Cassidy <steve@fieldnote.au>

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -26,16 +26,11 @@ import react from '@vitejs/plugin-react-swc';
 // not for the production build
 const global = process.env.NODE_ENV === 'development' ? 'window' : 'global';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const config: any = {
   base: '/',
   build: {
     outDir: 'build',
   },
-  // server: {
-  //   port: 3000,
-  //   host: true,
-  // },
   server: {
     host: '0.0.0.0',
     port: 3000,
@@ -56,6 +51,7 @@ export default defineConfig({
   define: {
     global: global,
     'process.env': {} /* some libraries check this */,
+    __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
   },
   test: {
     globals: true,
@@ -69,4 +65,16 @@ export default defineConfig({
       exclude: [],
     },
   },
+};
+
+// Conditional configuration.  If run with --mode sourcemap
+// we will build with sourcemaps enabled and output to a different directory.
+export default defineConfig(({mode}) => {
+  if (mode === 'sourcemap') {
+    config.build.sourcemap = true;
+    config.build.outDir = 'build-sourcemap';
+    return config;
+  } else {
+    return config;
+  }
 });
