02c562465572aa413ab07814f0aec3e90e071eb4
Steve Cassidy <steve@fieldnote.au>
2025-08-22 15:44:53 +1000

    add sourcemap upload step to CI app builds
    Signed-off-by: Steve Cassidy <steve@fieldnote.au>

--- app/bin/bugsnag-sourcemaps.sh
+++ app/bin/bugsnag-sourcemaps.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-# upload sourcemaps to bugsnag
+# upload sourcemaps to bugsnag if we have a key
 
-npx bugsnag-source-maps upload-browser \
-  --api-key ${VITE_BUGSNAG_KEY} \
-  --detect-app-version \
-  --base-url ${VITE_CONDUCTOR_URL}/assets \
-  --directory ./build-sourcemap/assets
+# BASE URL for app deployment is localhost, override with environment setting if present
+BASE_URL="${VITE_APP_URL:-https://localhost}"
+
+if [ -n "$VITE_BUGSNAG_KEY" ]; then
+  # build the sourcemap version of the app
+  npm run build-sourcemap
+  npx bugsnag-source-maps upload-browser \
+    --api-key ${VITE_BUGSNAG_KEY} \
+    --detect-app-version \
+    --base-url ${BASE_URL}/assets \
+    --directory ./build-sourcemap/assets
+fi
+
