786c94f1861e35900a02ea6da906c60b8b9baf2f
Peter Baker <peter.baker122@csiro.au>
2025-11-04 12:26:59 +1100

    Removing create element
    Signed-off-by: Peter Baker <peter.baker122@csiro.au>

--- library/forms/lib/components/Field/index.tsx
+++ library/forms/lib/components/Field/index.tsx
@@ -1,4 +1,4 @@
-import React, {createElement} from 'react';
+import React from 'react';
 import {EncodedFieldSpecification, FaimsForm} from '../../types';
 import {getFieldInfo} from '../fields';
 
@@ -17,20 +17,22 @@ export const Field = React.memo((props: FieldProps) => {
     namespace: props.fieldSpec['component-namespace'],
     name: props.fieldSpec['component-name'],
   });
+
   if (!fieldInfo) {
     throw new Error(
       `Field type ${props.fieldSpec['component-namespace']}::${props.fieldSpec['component-name']} not registered`
     );
   }
+
+  // Rename here to prompt tsx it's a component
+  const Component = fieldInfo.component;
+
   return (
     <props.form.Field
       name={props.fieldSpec['component-parameters'].name}
-      children={field => {
-        return createElement(fieldInfo!.component, {
-          ...props.fieldSpec['component-parameters'],
-          field: field,
-        });
-      }}
+      children={field => (
+        <Component {...props.fieldSpec['component-parameters']} field={field} />
+      )}
     />
   );
 });
