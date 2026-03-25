230879586d08e30d585b5ad280db732ba6442fb6
Ranisa Gupta <gup042@csiro.au>
2026-03-25 13:34:43 +1100

    update checkbox default
    Signed-off-by: Ranisa Gupta <gup042@csiro.au>

--- web/src/designer/fields.tsx
+++ web/src/designer/fields.tsx
@@ -218,7 +218,7 @@ const fields: {[key: string]: FieldType} = {
         multiple: true,
       },
       ElementProps: {
-        expandedChecklist: false,
+        expandedChecklist: true,
         enableOtherOption: false,
         options: [
           {value: 'Default', label: 'Default'},
