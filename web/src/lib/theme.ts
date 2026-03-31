04f70b5d5f4685ad028641e5b1b97938af5f1d11
Ranisa Gupta <gup042@csiro.au>
2026-04-01 10:54:29 +1100

    introduced new dass theme to align with project name
    Signed-off-by: Ranisa Gupta <gup042@csiro.au>

--- web/src/lib/theme.ts
+++ web/src/lib/theme.ts
@@ -2,6 +2,9 @@ export const THEME = import.meta.env.VITE_THEME || 'default';
 
 export const getThemeClass = () => {
   switch (THEME) {
+    case 'dass':
+    case 'dassTheme':
+      return 'theme-dass';
     case 'bssTheme':
       return 'theme-bss';
     case 'default':
