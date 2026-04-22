7b486e04c080ba1b85380b4f03eaf295f4fd09dc
Ranisa Gupta <gup042@csiro.au>
2026-04-23 09:14:37 +1000

    add DesignerThemeTokens type skeleton with core palette fields
    Signed-off-by: Ranisa Gupta <gup042@csiro.au>

---
+++ web/src/designer/theme/tokens.ts
@@ -0,0 +1,13 @@
+/**
+ * Canonical design-token contract shared by every designer theme variant.
+ * Each theme file must satisfy this type.
+ */
+export type DesignerThemeTokens = {
+  backgroundDefault: string;
+
+  primaryMain: string;
+  primaryLight: string;
+  primaryDark: string;
+  primaryContrastText: string;
+  secondaryMain: string;
+};
