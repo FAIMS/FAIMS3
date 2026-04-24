97a25f89bc9ee2240d116c067e45f9f562ce4e26
Ranisa Gupta <gup042@csiro.au>
2026-04-24 15:32:18 +1000

    add base FAIMS designer token set
    Signed-off-by: Ranisa Gupta <gup042@csiro.au>

---
+++ web/src/designer/theme/faims-tokens.ts
@@ -0,0 +1,25 @@
+import {colors} from '@mui/material';
+import type {DesignerThemeTokens} from './tokens';
+
+export const faimsTokens: DesignerThemeTokens = {
+  backgroundDefault: '#FAFAFB',
+  primaryMain: '#669911',
+  primaryLight: '#a7e938',
+  primaryDark: '#141E03',
+  primaryContrastText: '#FFFFFF',
+  secondaryMain: '#E18200',
+  helperTextColor: colors.blueGrey[500],
+  appBarBackground: '#edeeeb',
+  appBarColor: '#324C08',
+  formTabBorderColor: '#E18200',
+  formTabSelectedBg: '#DA9449',
+  formTabSelectedText: '#FFFFFF',
+  formTabIndicatorVisible: false,
+  formTabIndicatorColor: '#E18200',
+  errorMain: '#D32F2F',
+  deleteButtonColor: '#D32F2F',
+  tooltipIconColor: '#1976D2',
+  darkGrey: colors.blueGrey[700],
+  midGrey: colors.blueGrey[500],
+  lightGrey: colors.blueGrey[100],
+};
