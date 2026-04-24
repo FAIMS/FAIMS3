6ce187bfe506d9e1b1716f2bf1ace0287beca489
Ranisa Gupta <gup042@csiro.au>
2026-04-24 15:18:42 +1000

    add base DASS designer token set
    Signed-off-by: Ranisa Gupta <gup042@csiro.au>

---
+++ web/src/designer/theme/dass-tokens.ts
@@ -0,0 +1,25 @@
+import {colors} from '@mui/material';
+import type {DesignerThemeTokens} from './tokens';
+
+export const dassTokens: DesignerThemeTokens = {
+  backgroundDefault: '#FAFAFB',
+  primaryMain: '#000000',
+  primaryLight: '#4A4A4A',
+  primaryDark: '#000000',
+  primaryContrastText: '#FFFFFF',
+  secondaryMain: '#C40000',
+  helperTextColor: colors.grey[600],
+  appBarBackground: '#FFFFFF',
+  appBarColor: '#000000',
+  formTabBorderColor: '#C40000',
+  formTabSelectedBg: '#000000',
+  formTabSelectedText: '#FFFFFF',
+  formTabIndicatorVisible: true,
+  formTabIndicatorColor: '#C40000',
+  errorMain: '#C40000',
+  deleteButtonColor: '#C40000',
+  tooltipIconColor: '#1565C0',
+  darkGrey: colors.grey[700],
+  midGrey: colors.grey[500],
+  lightGrey: colors.grey[200],
+};
