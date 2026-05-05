9aee39b5d1b81e5fa645cf569779583de63eb762
Ranisa Gupta <gup042@csiro.au>
2026-05-05 18:29:08 +1000

    new field changes, improvmeents to basefieldeditor, fieldwrapper udpated after testing
    Signed-off-by: Ranisa Gupta <gup042@csiro.au>

--- web/src/designer/components/Fields/SimpleFieldWrapper.tsx
+++ web/src/designer/components/Fields/SimpleFieldWrapper.tsx
@@ -2,7 +2,7 @@ import {Box, Typography} from '@mui/material';
 
 type SimpleFieldWrapperProps = {
   heading: string;
-  helperText?: string;
+  helperText?: React.ReactNode;
   children: React.ReactNode;
 };
 
