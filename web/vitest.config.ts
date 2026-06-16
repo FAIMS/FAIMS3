372e08c89b08f35c44c910d7d26adecf134d1dd7
Steve Cassidy <steve@fieldnote.au>
2026-06-16 18:34:45 +1000

    fix up tests in web
    Signed-off-by: Steve Cassidy <steve@fieldnote.au>

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -1,11 +1,16 @@
 import {defineConfig} from 'vitest/config';
 import {webdriverio} from '@vitest/browser-webdriverio';
+import path from 'path';
 
 export default defineConfig({
   define: {
     global: 'globalThis',
     // Replace __APP_VERSION__ with package.json version at build time
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version),
+  },
+  resolve: {
+    alias: {'@': path.resolve(__dirname, './src')},
+    dedupe: ['@mui/material', '@emotion/react', '@emotion/styled'],
   },
   test: {
     environment: 'jsdom',
