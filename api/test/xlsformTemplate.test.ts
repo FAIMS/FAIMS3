54b9fc05cdf300fe0c236c777cdd9a6fe42de0aa
anesuc21 <w.a.chibowora@gmail.com>
2026-08-20 16:50:47 +1000

    xlsform test updated

--- api/test/xlsformTemplate.test.ts
+++ api/test/xlsformTemplate.test.ts
@@ -1,24 +1,3 @@
-/*
- * Copyright 2021, 2022 Macquarie University
- *
- * Licensed under the Apache License Version 2.0 (the, "License");
- * you may not use, this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing software
- * distributed under the License is distributed on an "AS IS" BASIS
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND either express or implied.
- * See, the License, for the specific language governing permissions and
- * limitations under the License.
- *
- * Filename: xlsformTemplate.test.ts
- * Description:
- *   Integration tests for POST /api/templates/from-xlsform -- the
- *   server-side XLSForm-to-Fieldmark template creation endpoint.
- */
-
 import PouchDB from 'pouchdb';
 import PouchDBFind from 'pouchdb-find';
 PouchDB.plugin(require('pouchdb-adapter-memory'));
